dc3bf8f480d9f42b28f1dc1a99420a240cc5411e
elegantiron <elegant.iron3167@fastmail.com>
2024-12-16 09:32:09 -0500

    update to handle new visible tile tag

--- game/utils.py
+++ game/utils.py
@@ -22,7 +22,9 @@ def new_tile(
     sprite_id: Sprites,
     dtype: npt.DTypeLike,
 ):
-    return np.array((walkable, transparent, False, False, sprite_id), dtype=dtype)
+    return np.array(
+        (walkable, transparent, False, False, False, sprite_id), dtype=dtype
+    )
 
 
 def get_damage_factor(
