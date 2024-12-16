d4682745271e6d0fafcb601fc4d3edb5e5f4e069
elegantiron <elegant.iron3167@fastmail.com>
2024-12-16 09:30:49 -0500

    formatting

--- game/definitions.py
+++ game/definitions.py
@@ -5,27 +5,31 @@ from numpy import dtype
 
 from game.constants import Sprites, Tile
 
+
 @dataclass
 class Color:
     """A class to handle colors"""
+
     r: int
     g: int
     b: int
     a: int
-    
+
     @property
     def rgb(self) -> tuple[int, int, int]:
         return self.r, self.g, self.b
-    
+
     @property
-    def rgba(self) -> tuple[int, int ,int, int]:
+    def rgba(self) -> tuple[int, int, int, int]:
         return *self.rgb, self.a
-    
+
+
 tile_dt = dtype(
     [
         (Tile.Walkable, bool),
         (Tile.Transparent, bool),
         (Tile.Explored, bool),
+        (Tile.Visible, bool),
         (Tile.Safe, bool),
         (Tile.SpriteID, Sprites),
     ]
