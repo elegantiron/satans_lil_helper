d976a98f939843e6d662ca201c2cf7c4da1eac9b
elegantiron <elegant.iron3167@fastmail.com>
2024-12-22 10:33:58 -0500

    add fog surface to sprites dictionary

--- game/setup.py
+++ game/setup.py
@@ -5,12 +5,19 @@ from pygame.freetype import Font
 
 from game.constants import FontDict, FONT_SETTINGS, Sprites, SPRITEPATHS
 
+
 def load_sprites() -> dict[Sprites, Surface]:
     data = {}
     for label in Sprites:
-        data.update({label: image.load(SPRITEPATHS[label]).convert_alpha()})
+        if label in SPRITEPATHS.keys():
+            data.update({label: image.load(SPRITEPATHS[label]).convert_alpha()})
+    fog_surface = Surface((32, 32))
+    fog_surface.fill((0x00, 0x00, 0x00))
+    fog_surface.set_alpha(0x50)
+    data.update({Sprites.FogTile: fog_surface})
 
     return data
+
 
 def load_fonts() -> dict[FontDict, Font]:
     data: dict[FontDict, Font] = {}
@@ -18,8 +25,8 @@ def load_fonts() -> dict[FontDict, Font]:
         path, size, fgcolor, bgcolor = FONT_SETTINGS[label]
         data.update({label: Font(path, size)})
         if fgcolor is not None:
-            data[label].fgcolor=fgcolor
+            data[label].fgcolor = fgcolor
         if bgcolor is not None:
-            data[label].bgcolor=bgcolor
+            data[label].bgcolor = bgcolor
 
     return data
