b026f510380d68101af60fe8dace2bacbbe38da3
elegantiron <elegant.iron3167@fastmail.com>
2024-12-23 16:05:32 -0500

    add wolf factory

--- game/entity_factories.py
+++ game/entity_factories.py
@@ -1,15 +1,32 @@
 from __future__ import annotations
+
 from typing import TYPE_CHECKING
 
-from game.professions import Profession
-from game.components import CoolDown, Inventory, Level, Name, Position, Renderable, Sight
-from game.constants import Sprites
+import tcod.ecs.entity
+
+from game.components import (
+    CoolDown,
+    EntityAI,
+    Health,
+    Inventory,
+    Level,
+    Name,
+    Position,
+    Renderable,
+    Sight,
+)
+from game.constants import EnemyType, Sprites
+
 
 if TYPE_CHECKING:
+    from random import Random
+    from game.professions import Profession
     import tcod.ecs
 
 
-def make_player(entity: tcod.ecs.Entity, profesion: Profession, position: tuple[int, int]):
+def make_player(
+    entity: tcod.ecs.Entity, profesion: Profession, position: tuple[int, int]
+):
     entity.components |= {
         Position: Position(*position),
         CoolDown: CoolDown(0),
@@ -18,4 +35,13 @@ def make_player(entity: tcod.ecs.Entity, profesion: Profession, position: tuple[
         Name: Name("Player"),
         Sight: Sight(6, 6),
         Level: Level(1, 0, 0),
+    }
+
+
+def make_wolf(entity: tcod.ecs.Entity, position: tuple[int, int], rng: Random) -> None:
+    entity.components |= {
+        Position: Position(*position),
+        Name: Name(EnemyType.Wolf),
+        Health: Health(rng.randint(1, 8) + 16),
+        EntityAI: EntityAI(EnemyType.Wolf),
     }
