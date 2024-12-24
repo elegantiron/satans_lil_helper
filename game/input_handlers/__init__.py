36280f4cdecaba0d4c87374b1fbe61582a1d9237
elegantiron <elegant.iron3167@fastmail.com>
2024-12-24 12:07:34 -0500

    add MainGameHandler to __all__

--- game/input_handlers/__init__.py
+++ game/input_handlers/__init__.py
@@ -2,10 +2,12 @@ from .bestiaryhandler import BestiaryInputHandler
 from .mainmenuhandler import MainMenuInputHandler
 from .gamemenuhandler import GameMenuInputHandler
 from .selectclasshandler import SelectClassInputHandler
+from .maingamehandler import MainGameInputHandler
 
 __all__ = [
     "BestiaryInputHandler",
     "MainMenuInputHandler",
     "GameMenuInputHandler",
-    "SelectClassInputHandler"
+    "SelectClassInputHandler",
+    "MainGameInputHandler"
 ]
