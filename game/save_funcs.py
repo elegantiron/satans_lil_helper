40672182ff42624df8826bf3f7424319abff480a
elegantiron <elegant.iron3167@fastmail.com>
2024-12-25 09:46:03 -0500

    put save data in a safe directory

--- game/save_funcs.py
+++ game/save_funcs.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 import hashlib
 import hmac
 import lzma
+import os
 
 import dill as pickle
 
@@ -18,17 +19,25 @@ def save_data(data, filename):
     signer = hmac.new(HMAC_KEY, digestmod=hashlib.blake2b)
     signer.update(save_data)
     mac_result = signer.digest()
-    with open(filename, "wb") as f:
+    path = "~/.slh/"
+    path = os.path.expanduser(path)
+    if not os.path.exists(path):
+        os.makedirs(path)
+    path = f"{path}{filename}"
+    print(path)
+    with open(path, "wb") as f:
         f.write(mac_result)
-    with open(filename, "ab") as f:
+    with open(path, "ab") as f:
         f.write(save_data)
 
 
 def load_data(filename):
     signer = hmac.new(HMAC_KEY, digestmod=hashlib.blake2b)
     save_data = ""
+    path = f"~/.slh/{filename}"
+    path = os.path.expanduser(path)
     try:
-        with open(filename, "rb") as f:
+        with open(path, "rb") as f:
             mac_data = f.read(signer.digest_size)
             save_data = f.read()
         signer.update(save_data)
