358bb9fe9ee1d2037a696fd1ab5f90ee0b1c9542
elegantiron <elegant.iron3167@fastmail.com>
2024-12-28 10:53:56 -0500

    add Name component

--- components/__init__.py
+++ components/__init__.py
@@ -5,7 +5,7 @@ import attrs
 from .position import Position
 from .stats import Growth, Stats
 
-__all__ = ["Position", "Growth", "Stats", "Attack"]
+__all__ = ["Position", "Growth", "Stats", "Attack", "Inventory", "Name"]
 
 
 @attrs.define
@@ -13,6 +13,12 @@ class Attack:
     dice: int
     sides: int
 
+
 @attrs.define
 class Inventory:
     size: int = 0
+
+
+@attrs.define
+class Name:
+    name: str
