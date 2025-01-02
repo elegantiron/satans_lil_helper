d2fa4b7e94fd6d0368afa2fb21f5bcfb85573945
elegantiron <elegant.iron3167@fastmail.com>
2025-01-02 07:56:22 -0500

    remove old build script

--- make.bat
+++
@@ -1,9 +0,0 @@
-if exist .venv/Scripts/activate.bat (
-    CALL .venv/Scripts/activate.bat
-) else (
-    python.exe -m venv .venv
-    CALL .venv/Scripts/activate.bat
-    python -m pip install -U pip
-)
-pip install -U -r requirements.txt
-pyinstaller -y -F -w -n "Satans Lil Helper" --add-data assets:assets main.py
