d2ffe1d73a26406295f3c5227869b56c459a37ab
elegantiron <elegant.iron3167@fastmail.com>
2025-01-03 10:31:35 -0500

    add equipment to warrior function

--- src/entities/professiosn.py
+++ src/entities/professiosn.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
-from components import Stats, Specials
+from components import Stats, Specials, Equipment
 from constants import SpecialAttacks, ActiveAbilities
 
 if TYPE_CHECKING:
@@ -10,5 +10,16 @@ if TYPE_CHECKING:
 def warrior_class(entity: tcod.ecs.Entity):
     entity.components |= {
         Stats: Stats(hp=50, mp=5, strength=5, pdef=7, mdef=2, sight=6, light=6),
-        Specials: Specials(attacks=[SpecialAttacks.Charge], skills=[ActiveAbilities.ShieldUp])
+        Specials: Specials(
+            attacks=[SpecialAttacks.Charge], skills=[ActiveAbilities.ShieldUp]
+        ),
+        Equipment: Equipment(
+            one_hand=True,
+            shield=True,
+            body=True,
+            head=True,
+            hands=True,
+            feet=True,
+            mundane=True,
+        ),
     }
