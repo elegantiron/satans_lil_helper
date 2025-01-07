9e973872aa6f5791efd55ef3e602ac3dd3f5b260
elegantiron <elegant.iron3167@fastmail.com>
2025-01-07 13:06:37 -0500

    add warrior level-up abilities

--- src/constants/abilities.py
+++ src/constants/abilities.py
@@ -41,3 +41,60 @@ class Repeatable(Enum):
 class Warrior(Flag):
     SHIELD_UP = auto()
     CHARGE = auto()
+    
+    FLAMING_WEAPON = auto()
+    IMPROVED_FLAMING_WEAPON = auto()
+    ELECTRIC_WEAPON = auto()
+    IMPROVED_ELECTRIC_WEAPON = auto()
+    COLD_WEAPON = auto()
+    IMPROVED_COLD_WEAPON = auto()
+    SERRATED_WEAPON = auto()
+    
+    SHIELD_WALL = auto()
+    SHIELD_WALL_TOSS = auto()
+    
+    IMPROVED_CHARGE = auto()
+    SHOVING_CHARGE = auto()
+    WALL_SLAM = auto()
+    
+    FIRE_RESISTANT = auto()
+    FIRE_BODY = auto()
+    GREATER_FIRE_BODY = auto()
+    
+    ELECTRIC_RESISTANT = auto()
+    SHOCK_COLLAR = auto()
+    IMPROVED_SHOCK_COLLAR = auto()
+    
+    COLD_RESISTANT = auto()
+    FROST_STEP = auto()
+    IMPROVED_FROST_STEP = auto()
+    
+    SHIELD_SLAM = auto()
+    
+    TARGET_HEAD = auto()
+    TARGET_LEG = auto()
+    TARGET_ARM = auto()
+    DISARM = auto()
+    
+    LEAPING_CHARGE = auto()
+    
+    ENCHANTED_SHIELD = auto()
+    RETRIBUTIVE_STRIKE = auto()
+    
+    SHIELD_SPELL_ABSORPTION = auto()
+    SHIELD_SPELL_RETRIBUTION = auto()
+    
+    FIELD_MEDICINE = auto()
+    SPLINT = auto()
+    FIELD_HOSPITAL = auto()
+    
+    SWORD_TRAINING = auto()
+    AXE_TRAINING = auto()
+    HAMMER_TRAINING = auto()
+    MACE_TRAINING = auto()
+    SPEAR_TRAINING = auto()
+    PICK_TRAINING = auto()
+    WHIP_TRAINING = auto()
+    SCYTHE_TRAINING = auto()
+    POLEARM_TRAINING = auto()
+    RAPIER_TRAINING = auto()
