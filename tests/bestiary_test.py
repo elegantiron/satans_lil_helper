7e4ed5c39daf83fefad0ea83021690045f6ffbab
elegantiron <elegant.iron3167@fastmail.com>
2025-01-24 09:20:06 -0500

    add bestiary tests

--- tests/bestiary_test.py
+++ tests/bestiary_test.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import pytest
 
 from bestiary import Bestiary
+from constants import tags
 
 
 @pytest.fixture(scope="class")
@@ -16,13 +17,19 @@ def bestiary2() -> Bestiary:
 
 
 class TestBestiary:
-    @pytest.mark.xfail(reason="test not written", raises=NotImplementedError)
-    def test_kills(self, bestiary):
-        raise NotImplementedError
+    def test_kills(self, bestiary: Bestiary, bestiary2: Bestiary) -> None:
+        for enemy_type in list(tags.Enemies):
+            for _ in range(50):
+                bestiary.record_kill(enemy_type=enemy_type, alpha=False)
+                bestiary2.record_kill(enemy_type=enemy_type, alpha=False)
+            for _ in range(25):
+                bestiary.record_kill(enemy_type=enemy_type, alpha=True)
+                bestiary2.record_kill(enemy_type=enemy_type, alpha=True)
 
-    @pytest.mark.xfail(reason="test not written", raises=NotImplementedError)
-    def test_get_stats(self, bestiary):
-        raise NotImplementedError
+    def test_get_stats(self, bestiary: Bestiary) -> None:
+        assert bestiary.total_murders() == 75 * len(tags.Enemies)
+        for enemy_type in list(tags.Enemies):
+            assert bestiary.total_murders(enemy_type) == 75
 
-    def test_comparison(self, bestiary, bestiary2):
+    def test_comparison(self, bestiary: Bestiary, bestiary2: Bestiary) -> None:
         assert bestiary == bestiary2
