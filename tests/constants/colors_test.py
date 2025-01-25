17509d12850f0dfe6492c7c322c8bd218f68db1d
elegantiron <elegant.iron3167@fastmail.com>
2025-01-25 08:51:01 -0500

    test that color values are in valid range

--- tests/constants/colors_test.py
+++ tests/constants/colors_test.py
@@ -1,10 +1,13 @@
 from __future__ import annotations
 
+import pytest
+
 from constants import Colors
 
 
-def test_colors():
-    for color in Colors:
+@pytest.mark.parametrize("color", Colors)
+def test_colors(color):
         assert isinstance(color, tuple)
         for hex_value in color:
             assert isinstance(hex_value, int)
+            assert 0 <= hex_value <= 255
