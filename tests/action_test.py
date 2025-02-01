26f65b19aed05639fce895018249fbfc320a1547
elegantiron <elegant.iron3167@fastmail.com>
2025-02-01 09:45:49 -0500

    test bump action

--- tests/action_test.py
+++ tests/action_test.py
@@ -4,7 +4,7 @@ from typing import TYPE_CHECKING, Final
 
 import pytest
 
-from actions import MeleeAction, MoveAction
+from actions import BumpAction, MeleeAction, MoveAction
 from components import Position, Stats
 from entities import enemies
 from exceptions import Impossible, OutOfBounds, PathBlocked
@@ -12,7 +12,7 @@ from gameworld import GameWorld
 from messagelog import MessageLog
 
 if TYPE_CHECKING:
-    import tcod.ecs
+    from tcod.ecs.entity import Entity
 
 SEED: Final = 1737855529.0953882
 
@@ -24,7 +24,7 @@ def gameworld() -> GameWorld:
 
 
 @pytest.fixture(scope="class")
-def entity(gameworld: GameWorld) -> tcod.ecs.Entity:
+def entity(gameworld: GameWorld) -> Entity:
     return gameworld.spawn_entity(
         enemies.forest.wolf,
         (
@@ -33,9 +33,11 @@ def entity(gameworld: GameWorld) -> tcod.ecs.Entity:
         ),
     )
 
+
 @pytest.fixture(scope="class")
 def message_log() -> MessageLog:
     return MessageLog()
+
 
 class TestActions:
     def test_movement(self, gameworld: GameWorld) -> None:
@@ -54,17 +56,30 @@ class TestActions:
             MoveAction(gameworld.player, (-500, 0), gameworld.map).perform()
         assert exc.type is OutOfBounds
 
-    def test_blocked_movement(
-        self, gameworld: GameWorld, entity: tcod.ecs.Entity
-    ) -> None:
+    def test_blocked_movement(self, gameworld: GameWorld, entity: Entity) -> None:
         assert entity is not None
         with pytest.raises(PathBlocked) as exc:
             MoveAction(gameworld.player, (0, 1), gameworld.map).perform()
         assert exc.type is PathBlocked
 
     @pytest.mark.depends(on=["tests/messagelog_test.py::TestMessageLog::test_logging"])
-    def test_melee_action(self, gameworld: GameWorld, entity: tcod.ecs.Entity, message_log: MessageLog) -> None:
+    def test_melee_action(
+        self, gameworld: GameWorld, entity: Entity, message_log: MessageLog
+    ) -> None:
         assert entity is not None
         old_hp = entity.components[Stats].hp
-        MeleeAction(gameworld.player, (0, 1), gameworld.map, gameworld.rng, message_log).perform()
+        MeleeAction(
+            gameworld.player, (0, 1), gameworld.map, gameworld.rng, message_log
+        ).perform()
         assert old_hp > entity.components[Stats].hp
+
+    @pytest.mark.depends(
+        on=["test_movement", "test_blocked_movement", "test_melee_action"]
+    )
+    def test_bump_action(
+        self, gameworld: GameWorld, entity: Entity, message_log: MessageLog
+    ) -> None:
+        old_hp = entity.components[Stats].hp
+        BumpAction(gameworld.player, (0, 1), gameworld.map, gameworld.rng, message_log).perform()
+        assert old_hp > entity.components[Stats].hp
+        BumpAction(gameworld.player, (1, 0), gameworld.map, gameworld.rng, message_log).perform()
