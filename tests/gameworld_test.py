75d5d88dabad59b02832fa49853f67d5dc038b21
elegantiron <elegant.iron3167@fastmail.com>
2025-02-06 08:12:37 -0500

    centralize more fixtures

--- tests/gameworld_test.py
+++ tests/gameworld_test.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import random
-from typing import TYPE_CHECKING, Final
+from typing import TYPE_CHECKING
 
 import pytest
 
@@ -9,8 +9,6 @@ from components import ActionDelay, Position
 from constants import Tile
 from entities import enemies, professions
 from exceptions import Impossible, SpawnBlocked
-from gameworld import GameWorld
-from messagelog import MessageLog
 
 if TYPE_CHECKING:
     from collections.abc import Callable
@@ -18,68 +16,56 @@ if TYPE_CHECKING:
 
     import tcod.ecs
 
-SEED: Final = 1737855529.0953882
-# pylint: disable=redefined-outer-name
+    from gameworld import GameWorld
 
-
-@pytest.fixture
-def gameworld() -> GameWorld:
-    return GameWorld()
-
-
-@pytest.fixture
-def gameworld2() -> GameWorld:
-    return GameWorld(SEED)
-
-
-@pytest.fixture
-def gameworld3() -> GameWorld:
-    return GameWorld(SEED)
-
-
-@pytest.fixture
-def message_log() -> MessageLog:
-    return MessageLog()
 
 @pytest.mark.slow
 @pytest.mark.depends(on="MessageLog", name="GameWorld")
 class TestGameWorld:
     @pytest.mark.parametrize("iteration", range(5))
-    def test_player_position(self, gameworld: GameWorld, iteration: int) -> None:  # pylint: disable=unused-argument
-        p_pos = gameworld.player.components[Position]
+    def test_player_position(
+        self, gameworld_random_seed: GameWorld, iteration: int
+    ) -> None:  # pylint: disable=unused-argument
+        p_pos = gameworld_random_seed.player.components[Position]
         assert p_pos is not None
-        assert gameworld.map.tiles[Tile.WALKABLE][p_pos.xy]
+        assert gameworld_random_seed.map.tiles[Tile.WALKABLE][p_pos.xy]
 
-    def test_equality(self, gameworld: GameWorld, gameworld2: GameWorld) -> None:
-        assert gameworld != gameworld2
-        assert gameworld2 == gameworld2
-        assert gameworld == gameworld
+    def test_equality(
+        self, gameworld_random_seed: GameWorld, gameworld_random_seed2: GameWorld
+    ) -> None:
+        assert gameworld_random_seed != gameworld_random_seed2
+        assert gameworld_random_seed2 == gameworld_random_seed2
+        assert gameworld_random_seed == gameworld_random_seed
 
     @pytest.mark.parametrize("spawn_function", [enemies.forest.wolf])
     def test_entity_spawn(
         self,
-        gameworld: GameWorld,
+        gameworld_random_seed: GameWorld,
         spawn_function: Callable[[tcod.ecs.Entity, tuple[int, int], Random], None],
     ) -> None:
         for _ in range(random.randint(4, 100)):
-            gameworld.spawn_entity(spawn_function)
+            gameworld_random_seed.spawn_entity(spawn_function)
         chosen = False
         x: int = 0
         y: int = 0
         while not chosen:
-            x = gameworld.rng.randint(0, len(gameworld.map.tiles) - 1)
-            y = gameworld.rng.randint(0, len(gameworld.map.tiles[x]) - 1)
-            if gameworld.map.tiles[Tile.WALKABLE][x, y]:
+            x = gameworld_random_seed.rng.randint(
+                0, len(gameworld_random_seed.map.tiles) - 1
+            )
+            y = gameworld_random_seed.rng.randint(
+                0, len(gameworld_random_seed.map.tiles[x]) - 1
+            )
+            if gameworld_random_seed.map.tiles[Tile.WALKABLE][x, y]:
                 chosen = True
-        gameworld.spawn_entity(spawn_function, (x, y))
+        gameworld_random_seed.spawn_entity(spawn_function, (x, y))
         with pytest.raises(Impossible) as exc:
-            gameworld.spawn_entity(spawn_function, (0, 0))
+            gameworld_random_seed.spawn_entity(spawn_function, (0, 0))
         assert exc.type is SpawnBlocked
 
-    def test_time_step(self, gameworld: GameWorld) -> None:
-        entity = gameworld.spawn_entity(professions.warrior_class)
+    def test_time_step(self, gameworld_random_seed: GameWorld) -> None:
+        entity = gameworld_random_seed.spawn_entity(professions.warrior_class)
         action_delay = entity.components.get(ActionDelay)
         assert action_delay is not None
         previous_delay = action_delay.ticks
-        gameworld.step_time()
+        gameworld_random_seed.step_time()
         assert action_delay.ticks == previous_delay - 1
