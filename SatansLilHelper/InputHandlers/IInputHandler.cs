e4d1bf8e07a34fb287b8c8a70b469a2b5d3a282e
elegantiron <elegant.iron3167@fastmail.com>
2025-03-17 19:21:48 -0400

    add IInputHandler interface

---
+++ SatansLilHelper/InputHandlers/IInputHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using SatansLilHelper.Constants;
+
+namespace SatansLilHelper.InputHandlers;
+
+public interface IInputHandler
+{
+    public abstract IInputHandler HandleKey(Keys key);
+
+    public abstract void Draw(
+        SpriteBatch spriteBatch,
+        Dictionary<TextureID, Texture2D> textureMap,
+        Dictionary<EffectID, SoundEffect> effectMap,
+        Dictionary<MusicID, Song> songMap,
+        Dictionary<FontID, SpriteFont> fontMap
+    );
+}
