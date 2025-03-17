9a2112a6a2b71d69bb7b705a98ca660a4d5101b6
elegantiron <elegant.iron3167@fastmail.com>
2025-03-17 19:03:00 -0400

    clear to black

--- SatansLilHelper/Engine.cs
+++ SatansLilHelper/Engine.cs
@@ -51,7 +51,7 @@ public class Engine : Game
 
     protected override void Draw(GameTime gameTime)
     {
-        GraphicsDevice.Clear(Color.CornflowerBlue);
+        GraphicsDevice.Clear(Color.Black);
 
         // TODO: Add your drawing code here
 
