7142c306152364ea7db3b4a05f96772eaab984aa
elegantiron <elegant.iron3167@fastmail.com>
2025-03-17 19:07:49 -0400

    Create Enums.cs

---
+++ SatansLilHelper/Constants/Enums.cs
@@ -0,0 +1,22 @@
+﻿namespace SatansLilHelper.Constants;
+
+public enum TextureID
+{
+    Player,
+    ForestFloor,
+    ForestWall,
+    SatanMain,
+    SatanEyesOpen,
+    SatanEyesClosed,
+    SatanMouthOpen,
+    SatanMouthClosed,
+}
+
+public enum MusicID
+{
+
+}
+
+public enum EffectID { }
+
+public enum FontID { }
