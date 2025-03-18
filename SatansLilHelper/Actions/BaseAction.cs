7efa5bd971948e0045cc71c91bcfb6450bf5bd2d
elegantiron <elegant.iron3167@fastmail.com>
2025-03-18 16:49:14 -0400

    Create BaseAction.cs

---
+++ SatansLilHelper/Actions/BaseAction.cs
@@ -0,0 +1,14 @@
+﻿using Friflo.Engine.ECS;
+
+namespace SatansLilHelper.Actions;
+
+public abstract class BaseAction(Entity entity, ArchetypeQuery query, bool isPlayer)
+{
+    protected Entity Entity = entity;
+    protected bool IsPlayer = isPlayer;
+    protected ArchetypeQuery Query = query;
+
+    public abstract void Perform();
+
+    public abstract void Rewind();
+}
