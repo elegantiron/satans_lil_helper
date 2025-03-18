7bf4bf9b7b9f20d3486193669f2563808169bd50
elegantiron <elegant.iron3167@fastmail.com>
2025-03-17 21:19:19 -0400

    Create ForestMap.cs

---
+++ SatansLilHelper/Utils/GameMaps/ForestMap.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Media;
+using SatansLilHelper.Constants;
+
+namespace SatansLilHelper.Utils.GameMaps;
+
+public class ForestMap(Point mapSize, Random rng) : BaseMap(mapSize, rng)
+{
+    public override void Draw(
+        SpriteBatch spriteBatch,
+        Dictionary<TextureID, Texture2D> textureMap,
+        Dictionary<EffectID, SoundEffect> effectMap,
+        Dictionary<MusicID, Song> songMap,
+        Dictionary<FontID, SpriteFont> fontMap
+    ) { }
+
+    public override void GenerateMap(Random rng, Point size) { }
+}
