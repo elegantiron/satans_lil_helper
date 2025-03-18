2322680a68dd961cee5510f0941e35bae74d2e91
elegantiron <128913032+elegantiron@users.noreply.github.com>
2025-03-18 13:49:01 -0400

    Create TitleInputHandler.cs

---
+++ SatansLilHelper/InputHandlers/TitleInputHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using SatansLilHelper.Constants;
+using SatansLilHelper.Utils;
+
+namespace SatansLilHelper.InputHandlers;
+
+public class TitleInputHandler : IInputHandler
+{
+    public void Draw(
+        SpriteBatch spriteBatch,
+        Dictionary<TextureID, Texture2D> textureMap,
+        Dictionary<EffectID, SoundEffect> effectMap,
+        Dictionary<MusicID, Song> songMap,
+        Dictionary<FontID, SpriteFont> fontMap
+    )
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public IInputHandler HandleKey(Keys key)
+    {
+        throw new System.NotImplementedException();
+    }
+}
