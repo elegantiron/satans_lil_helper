7d41a5c09536d8c0b8fb9d0e3ce8c02301637984
elegantiron <128913032+elegantiron@users.noreply.github.com>
2025-03-18 13:47:39 -0400

    Create GameWorld.cs

---
+++ SatansLilHelper/Utils/GameWorld.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using SatansLilHelper.Utils.GameMaps;
+
+namespace SatansLilHelper.Utils;
+
+public class GameWorld
+{
+    private List<BaseMap> maps;
+    private int currentMap = 0;
+    private Random rng;
+    private Point mapSize;
+
+    public GameWorld(Random rng, Point mapSize)
+    {
+        this.rng = rng;
+        this.mapSize = mapSize;
+        maps = [new ForestMap(this.mapSize, this.rng, new Point(1280,720))];
+    }
+
+    public BaseMap CurrentMap
+    {
+        get { return maps[currentMap]; }
+    }
+}
