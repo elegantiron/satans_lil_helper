c3910da6f62fe02166d3d4c257f776e058aeec32
elegantiron <elegant.iron3167@fastmail.com>
2025-03-17 21:19:10 -0400

    Create Professions.cs

---
+++ SatansLilHelper/Entities/Professions.cs
@@ -0,0 +1,12 @@
+﻿using Friflo.Engine.ECS;
+using SatansLilHelper.Components;
+
+namespace SatansLilHelper.Entities;
+
+public static class Professions
+{
+    public static void Warrior(Entity entity)
+    {
+        entity.AddComponent(new ActionDelay { value = 0 });
+    }
+}
