cfc3bb6a1479369d0693f87d6359be31628e8222
elegantiron <elegant.iron3167@fastmail.com>
2025-03-18 16:47:58 -0400

    Create Constants.cs

---
+++ SatansLilHelper/Constants.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace SatansLilHelper;
+
+public static class Constants
+{
+    public static readonly (int x, int y)[] NeighborDirections =
+    [
+        (-1, -1),
+        (-1, 0),
+        (-1, 1),
+        (0, -1),
+        (0, 1),
+        (1, -1),
+        (1, 0),
+        (1, 1),
+    ];
+
+    public static readonly Dictionary<Keys, Point> MovementKeys = new()
+    {
+        { Keys.Up, new Point(0, -1) },
+        { Keys.Down, new Point(0, 1) },
+        { Keys.Right, new Point(1, 0) },
+        { Keys.Left, new Point(-1, 0) },
+        { Keys.NumPad1, new Point(-1, 1) },
+        { Keys.NumPad2, new Point(0, 1) },
+        { Keys.NumPad3, new Point(1, 1) },
+        { Keys.NumPad4, new Point(-1, 0) },
+        { Keys.NumPad6, new Point(1, 0) },
+        { Keys.NumPad7, new Point(-1, -1) },
+        { Keys.NumPad8, new Point(0, -1) },
+        { Keys.NumPad9, new Point(1, -1) },
+    };
+}
