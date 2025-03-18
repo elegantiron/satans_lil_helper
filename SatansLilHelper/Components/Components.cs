030aeb1be9911ab2fc7dac9903c08bed6dd7f783
elegantiron <elegant.iron3167@fastmail.com>
2025-03-18 16:49:08 -0400

    Update Components.cs

--- SatansLilHelper/Components/Components.cs
+++ SatansLilHelper/Components/Components.cs
@@ -7,3 +7,21 @@ public struct ActionDelay : IComponent
 {
     public int value;
 }
+
+[ComponentKey("location")]
+public struct Location : IIndexedComponent<(int, int)>
+{
+    public int X,
+        Y;
+
+    public Location(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public (int, int) GetIndexedValue()
+    {
+        return (X, Y);
+    }
+}
