affa19c583435317e393eeb4b9e91c7b1f831433
elegantiron <128913032+elegantiron@users.noreply.github.com>
2025-03-18 10:10:22 -0400

    add initial generation logic

--- SatansLilHelper/Utils/GameMaps/ForestMap.cs
+++ SatansLilHelper/Utils/GameMaps/ForestMap.cs
@@ -18,5 +18,49 @@ public class ForestMap(Point mapSize, Random rng) : BaseMap(mapSize, rng)
         Dictionary<FontID, SpriteFont> fontMap
     ) { }
 
-    public override void GenerateMap(Random rng, Point size) { }
+    public override void GenerateMap(Random rng, Point size)
+    {
+        bool[,] tempMap = new bool[mapSize.X, mapSize.Y];
+        for (int i = 0; i < mapSize.X; i++)
+        {
+            for (int j = 0; j < mapSize.Y; j++)
+            {
+                tempMap[i, j] = rng.NextDouble() < 0.35f;
+            }
+        }
+
+        for (int iters = 0; iters < 5; iters++)
+        {
+            bool[,] newMap = tempMap;
+            for (int i = 0; i < mapSize.X; i++)
+            {
+                for (int j = 0; j < mapSize.Y; j++)
+                {
+                    int neighbors = GetNeighbors(new Point(i, j));
+                    if (neighbors < 4)
+                        newMap[i, j] = false;
+                    else if (neighbors > 4)
+                        newMap[i, j] = true;
+                }
+            }
+            tempMap = newMap;
+        }
+        for (int i = 0; i < mapSize.X; i++)
+        {
+            for (int j = 0; j < mapSize.Y; j++)
+            {
+                Tile tile;
+                if (tempMap[i, j])
+                    tile = new Tile(TextureID.ForestWall, false, false);
+                else
+                    tile = new Tile(TextureID.ForestFloor, true, true);
+                tiles[i, j] = tile;
+            }
+        }
+    }
+
+    private int GetNeighbors(Point target)
+    {
+        return 0;
+    }
 }
