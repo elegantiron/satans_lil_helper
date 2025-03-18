d7e57cf7b710e4ed7719d9cfd4af28a473939dd9
elegantiron <elegant.iron3167@fastmail.com>
2025-03-18 16:12:00 -0400

    move file

---
+++ SatansLilHelper/Types.cs
@@ -0,0 +1,14 @@
+﻿namespace SatansLilHelper;
+
+public static class Types
+{
+    public struct Tile(TextureID texture, bool walkable, bool passesLight)
+    {
+        public TextureID Texture = texture;
+        public bool Walkable = walkable;
+        public bool PassesLight = passesLight;
+        public bool Visible = false;
+        public bool Explored = false;
+        public float LightDistance = 0f;
+    }
+}
