f3f87517b2f519be7ff4e61c866204173a88cb97
elegantiron <elegant.iron3167@fastmail.com>
2025-03-17 20:08:03 -0400

    add cell grid interface

---
+++ SatansLilHelper/Utils/GameMaps/ICellGrid.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Friflo.Engine.ECS;
+using Microsoft.Xna.Framework;
+using SatansLilHelper.Constants;
+
+namespace SatansLilHelper.Utils.GameMaps;
+
+interface ICellGrid
+{
+    #region Methods
+    public bool IsWall(Point tile);
+    public void SetLight(Point tile, float distanceSquared);
+    public void GenerateMap(Random rng, Point size);
+    #endregion Methods
+
+    #region Properties
+    public int XDim { get; }
+    public int YDim { get; }
+    public Entity Player { get; }
+    public EntityStore Registry { get; }
+    public Tile[,] Tiles { get; }
+    #endregion Properties
+}
