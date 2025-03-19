c210e1da1aaa5af9dc979645958d39808bfc4d49
elegantiron <elegant.iron3167@fastmail.com>
2025-03-19 16:54:02 -0400

    Update MersenneTwister.cs

--- SatansLilHelper/Utils/MersenneTwister.cs
+++ SatansLilHelper/Utils/MersenneTwister.cs
@@ -1,22 +1,32 @@
 ﻿using System;
+using MessagePack;
 
 namespace SatansLilHelper.Utils;
 
 public class MersenneTwister
 {
     private const int N = 624;
+
     private const int M = 397;
+
     private const uint MatrixA = 0x9908b0df;
+
     private const uint UpperMask = 0x80000000;
+
     private const int LowerMask = 0x7fffffff;
+
     private const uint SpectralTestMultiplier = 1812433253;
 
     private const uint B = 0x9d2c5680;
+
     private const uint C = 0xefc60000;
 
     private static readonly uint[] Mag01 = [0, MatrixA];
 
     private static readonly uint[] _state = new uint[N];
+    private MessagePackSerializerOptions lz4Options =
+        MessagePackSerializerOptions.Standard.WithCompression(MessagePackCompression.Lz4Block);
+
     private int _index = N + 1;
 
     public MersenneTwister()
@@ -83,5 +93,20 @@ public class MersenneTwister
         next ^= (next >> 18);
 
         return next;
+    }
+
+    public byte[] GetState()
+    {
+        byte[] state = MessagePackSerializer.Serialize((_state, _index), lz4Options);
+        return state;
+    }
+
+    public void SetState(byte[] state)
+    {
+        (uint[] numberList, _index) = MessagePackSerializer.Deserialize<(uint[], int)>(
+            state,
+            lz4Options
+        );
+        numberList.CopyTo(_state, 0);
     }
 }
