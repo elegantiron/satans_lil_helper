d4f43dd5ae1d1204a57bacdb3961bcd960d3053b
elegantiron <128913032+elegantiron@users.noreply.github.com>
2025-03-19 09:49:16 -0400

    Update Constants.cs

--- SatansLilHelper/Utils/Constants.cs
+++ SatansLilHelper/Utils/Constants.cs
@@ -24,6 +24,7 @@ public static class Constants
         { Keys.Down, new Point(0, 1) },
         { Keys.Right, new Point(1, 0) },
         { Keys.Left, new Point(-1, 0) },
+        { Keys.Insert, new Point(-1, -1) },
         { Keys.NumPad1, new Point(-1, 1) },
         { Keys.NumPad2, new Point(0, 1) },
         { Keys.NumPad3, new Point(1, 1) },
@@ -32,5 +33,8 @@ public static class Constants
         { Keys.NumPad7, new Point(-1, -1) },
         { Keys.NumPad8, new Point(0, -1) },
         { Keys.NumPad9, new Point(1, -1) },
+        { Keys.Delete, new Point(-1, 1) },
+        { Keys.PageUp, new Point(1, -1) },
+        { Keys.PageDown, new Point(1, 1) },
     };
 }
