25faf544edc76dd9344f4a18463193dd9055a1f4
elegantiron <elegant.iron3167@fastmail.com>
2025-03-18 20:25:22 -0400

    Create ActionStack.cs

---
+++ SatansLilHelper/Utils/ActionStack.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using SatansLilHelper.Actions;
+
+namespace SatansLilHelper.Utils;
+
+public class ActionStack
+{
+    private Stack<BaseAction> history = new(),
+        future = new();
+
+    public void AddAction(BaseAction action)
+    {
+        future.Clear();
+        action.Perform();
+        history.Push(action);
+    }
+
+    public void Rewind()
+    {
+        if (history.Count == 0)
+            return;
+        BaseAction action = history.Pop();
+        action.Rewind();
+        future.Push(action);
+    }
+
+    public void Replay()
+    {
+        if (future.Count == 0)
+            return;
+        BaseAction action = future.Pop();
+        action.Perform();
+        history.Push(action);
+    }
+}
