51403d9b1aa2cd4eafffa32f2e60581f20d5ca5d
elegantiron <128913032+elegantiron@users.noreply.github.com>
2025-03-19 12:19:47 -0400

    Create MessageLog.cs

---
+++ SatansLilHelper/Utils/MessageLog.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace SatansLilHelper.Utils;
+
+public class MessageLog
+{
+    private List<Message> Messages;
+
+    private class Message(string text, Color color)
+    {
+        private string Text = text;
+        private Color Color = color;
+        private int Count = 0;
+        public string PlainText
+        {
+            get { return Text; }
+        }
+
+        public void Stack()
+        {
+            Count++;
+        }
+    }
+
+    public void AddMessage(string text, Color color, bool stack = true)
+    {
+        if (stack && Messages[-1].PlainText == text)
+            Messages[-1].Stack();
+        else
+            Messages.Add(new Message(text, color));
+    }
+}
