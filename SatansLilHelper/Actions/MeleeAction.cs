fe8298d53566bb70810cfc82503877fa6aedc533
elegantiron <elegant.iron3167@fastmail.com>
2025-03-19 16:54:26 -0400

    Update MeleeAction.cs

--- SatansLilHelper/Actions/MeleeAction.cs
+++ SatansLilHelper/Actions/MeleeAction.cs
@@ -1,27 +1,38 @@
 ﻿using System;
 using Friflo.Engine.ECS;
 using Microsoft.Xna.Framework;
+using SatansLilHelper.Components;
 using SatansLilHelper.Exceptions;
+using SatansLilHelper.Utils;
 using SatansLilHelper.Utils.GameMaps;
 
 namespace SatansLilHelper.Actions;
 
 internal class MeleeAction : ActionWithDirection
 {
+    private byte[] preState,
+        postState;
+
     public MeleeAction(
         Entity entity,
         Point direction,
         BaseMap gameMap,
         ArchetypeQuery query,
-        Random rng,
+        MersenneTwister rng,
         bool isPlayer
     )
         : base(entity, direction, gameMap, query, isPlayer)
     {
-        if (!IsBlocked || TargetEntity == null)
-        {
+        if (!IsBlocked)
             throw new MissingTargetException();
-        }
+        if (TargetEntity == null)
+            throw new MissingTargetException();
+
+        if (!Entity.TryGetComponent<Attack>(out Attack aggressorAtk))
+            throw new Exceptions.MissingComponentException();
+        if (!(bool)TargetEntity?.TryGetComponent<Defense>(out Defense targetDef))
+            throw new Exceptions.MissingComponentException();
+        preState = rng.GetState();
     }
 
     public override void Perform()
