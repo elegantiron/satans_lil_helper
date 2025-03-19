8beed614dbe02788b9eed8cb38736f680fa80db0
elegantiron <128913032+elegantiron@users.noreply.github.com>
2025-03-19 15:06:24 -0400

    Create MeleeAction.cs

---
+++ SatansLilHelper/Actions/MeleeAction.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Friflo.Engine.ECS;
+using Microsoft.Xna.Framework;
+using SatansLilHelper.Exceptions;
+using SatansLilHelper.Utils.GameMaps;
+
+namespace SatansLilHelper.Actions;
+
+internal class MeleeAction : ActionWithDirection
+{
+    public MeleeAction(
+        Entity entity,
+        Point direction,
+        BaseMap gameMap,
+        ArchetypeQuery query,
+        Random rng,
+        bool isPlayer
+    )
+        : base(entity, direction, gameMap, query, isPlayer)
+    {
+        if (!IsBlocked || TargetEntity == null)
+        {
+            throw new MissingTargetException();
+        }
+    }
+
+    public override void Perform()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Rewind()
+    {
+        throw new NotImplementedException();
+    }
+}
