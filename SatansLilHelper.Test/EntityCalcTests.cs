bb22e66ec08e1a59bb4145b6fabe7a28a62c44d0
elegantiron <elegant.iron3167@fastmail.com>
2025-03-20 21:38:23 -0400

    Create EntityCalcTests.cs

---
+++ SatansLilHelper.Test/EntityCalcTests.cs
@@ -0,0 +1,12 @@
+﻿using SatansLilHelper.Utils;
+namespace SatansLilHelper.Test;
+
+internal class EntityCalcTests
+{
+    [Test]
+    public async Task Mytest()
+    {
+        var result = EntityCalcs.DoNothing();
+        await Assert.That(result).IsEqualTo(0);
+    }
+}
