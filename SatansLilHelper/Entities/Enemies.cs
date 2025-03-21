514585a079ab50f0d44eb427d9473ed1497e23bd
elegantiron <128913032+elegantiron@users.noreply.github.com>
2025-03-21 14:34:47 -0400

    Update Enemies.cs

--- SatansLilHelper/Entities/Enemies.cs
+++ SatansLilHelper/Entities/Enemies.cs
@@ -1,3 +1,22 @@
-﻿namespace SatansLilHelper.Entities;
+﻿using Friflo.Engine.ECS;
+using SatansLilHelper.Components;
+using SatansLilHelper.Utils;
 
-public static class Enemies { }
+namespace SatansLilHelper.Entities;
+
+public static class Enemies
+{
+    public static void Wolf(MersenneTwister rng, Entity entity)
+    {
+        entity.AddComponent(new EntityName("wolf"));
+        entity.AddComponent(new ActionDelay(rng.Next(1, 15)));
+
+        entity.AddRelation(new ResourceStat(ResourceID.Health, rng.Next(1, 8) + 16));
+
+        entity.AddRelation(new AbilityStat(AbilityID.Strength, 2));
+        entity.AddRelation(new AbilityStat(AbilityID.PhysicalDefense, 5));
+        entity.AddRelation(new AbilityStat(AbilityID.Crit, 1));
+        entity.AddRelation(new AbilityStat(AbilityID.Speed, 3));
+        entity.AddRelation(new AbilityStat(AbilityID.Vision, 8));
+    }
+}
