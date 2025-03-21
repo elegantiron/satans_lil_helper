8a6d0c0f980687910670749ac3beb29a3dbf0d7c
elegantiron <elegant.iron3167@fastmail.com>
2025-03-21 17:21:59 -0400

    Create EntityTestData.cs

---
+++ SatansLilHelper.Test/EntityTests/EntityTestData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using SatansLilHelper.Utils;
+
+namespace SatansLilHelper.Test.EntityTests;
+
+public static class EntityTestData
+{
+    public static IEnumerable<ResourceID> ResourceIDs()
+    {
+        foreach (ResourceID id in Enum.GetValues(typeof(ResourceID)))
+        {
+            yield return id;
+        }
+    }
+
+    public static IEnumerable<AbilityID> AbilityIDs()
+    {
+        foreach (AbilityID id in Enum.GetValues(typeof(AbilityID)))
+        {
+            yield return id;
+        }
+    }
+}
