00e9ea26ab789f228bf5b083b62183dda524f423
elegantiron <elegant.iron3167@fastmail.com>
2025-03-23 14:22:02 -0400

    Create EventBus.cs

---
+++ SatansLilHelper/Utils/EventBus.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SatansLilHelper.Utils;
+
+public static class EventBus
+{
+    private static Dictionary<ChannelKey, object> Listeners { get; } = [];
+
+    public static void Send<T>(object key, T arguments)
+    {
+        var fullKey = new ChannelKey(key, typeof(T));
+        if (!Listeners.TryGetValue(fullKey, out object value))
+            return;
+
+        var actionList = (List<EventReaction<T>>)value;
+
+        foreach (var listener in actionList)
+            listener.Reaction(arguments);
+    }
+
+    public static void Subscribe<T>(this object subscriber, object key, Action<T> listenerAction)
+    {
+        var fullKey = new ChannelKey(key, typeof(T));
+        if (!Listeners.ContainsKey(fullKey))
+            Listeners[fullKey] = new List<EventReaction<T>>();
+
+        var list = (List<EventReaction<T>>)Listeners[fullKey];
+        if (list.Any(reaction => reaction.Subscriber.Equals(subscriber)))
+            return;
+        list.Add(new EventReaction<T>(subscriber, listenerAction));
+    }
+
+    public static void UnSubscribe<T>(this object subscriber, object key)
+    {
+        var fullKey = new ChannelKey(key, typeof(T));
+        if (!Listeners.TryGetValue(fullKey, out object value))
+            return;
+        ((List<EventReaction<T>>)value).RemoveAll(listener =>
+            listener.Subscriber.Equals(subscriber)
+        );
+    }
+
+    private class ChannelKey(object key, Type argumentType = null)
+    {
+        private Type ArgumentType { get; } = argumentType;
+        private object Key { get; } = key;
+
+        public override bool Equals(object @object)
+        {
+            return @object is ChannelKey anotherKey
+                && anotherKey.Key.Equals(Key)
+                && anotherKey.ArgumentType == ArgumentType;
+        }
+
+        public override int GetHashCode()
+        {
+            return (Key?.GetHashCode() ?? 0) + (ArgumentType?.GetHashCode() ?? 0);
+        }
+    }
+
+    private class EventReaction<T>(object subscriber, Action<T> reaction)
+    {
+        public Action<T> Reaction { get; } = reaction;
+        public object Subscriber { get; } = subscriber;
+    }
+}
