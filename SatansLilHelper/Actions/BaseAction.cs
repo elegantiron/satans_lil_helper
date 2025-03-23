7b479033d0bdc06aad0fb9d2a663421981939bd8
elegantiron <elegant.iron3167@fastmail.com>
2025-03-23 13:47:05 -0400

    Update BaseAction.cs

--- SatansLilHelper/Actions/BaseAction.cs
+++ SatansLilHelper/Actions/BaseAction.cs
@@ -1,4 +1,5 @@
 ﻿using Friflo.Engine.ECS;
+using SatansLilHelper.Utils;
 
 namespace SatansLilHelper.Actions;
 
@@ -7,8 +8,17 @@ public abstract class BaseAction(Entity entity, ArchetypeQuery query, bool isPla
     protected Entity Entity = entity;
     protected bool IsPlayer = isPlayer;
     protected ArchetypeQuery Query = query;
+    protected LogMessage? _logMessage;
 
-    public abstract void Perform();
+    public virtual void Perform()
+    {
+        if (_logMessage != null)
+            EventBus.Send(Events.AddLogMessage, _logMessage ?? default);
+    }
 
-    public abstract void Rewind();
+    public virtual void Rewind()
+    {
+        if (_logMessage != null)
+            EventBus.Send(Events.PruneLogMessage, _logMessage ?? default);
+    }
 }
