8b52a5f5fafda1683c75a5b4afe4b9c96724364d
elegantiron <elegant.iron3167@fastmail.com>
2025-03-23 14:22:11 -0400

    Update Types.cs

--- SatansLilHelper/Utils/Types.cs
+++ SatansLilHelper/Utils/Types.cs
@@ -25,3 +25,10 @@ public struct TextVecs
         Location = Origin = Vector2.Zero;
     }
 }
+
+public struct LogMessage(string message, Color color, bool stack = true)
+{
+    public string Message = message;
+    public Color Color = color;
+    public bool Stack = stack;
+}
