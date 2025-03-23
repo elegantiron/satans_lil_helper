fde3395af0553648c21ef44e250bfea1acc01c2c
elegantiron <elegant.iron3167@fastmail.com>
2025-03-23 13:47:08 -0400

    Update MeleeAction.cs

--- SatansLilHelper/Actions/MeleeAction.cs
+++ SatansLilHelper/Actions/MeleeAction.cs
@@ -3,6 +3,7 @@ using System.Security.Cryptography;
 using Friflo.Engine.ECS;
 using Microsoft.Xna.Framework;
 using SatansLilHelper.Components;
+using SatansLilHelper.Content.Text;
 using SatansLilHelper.Exceptions;
 using SatansLilHelper.Utils;
 using SatansLilHelper.Utils.GameMaps;
@@ -29,12 +30,18 @@ internal class MeleeAction : ActionWithDirection
         if (!IsBlocked || TargetEntity == null)
             throw new MissingTargetException();
 
-
         Twister = rng;
         preState = rng.GetState();
 
         Damage = EntityCalcs.GetDamage(Twister, Entity, (Entity)TargetEntity);
-
+        _logMessage = new(
+            string.Format(
+                GameStrings.PlayerAttack,
+                TargetEntity?.GetComponent<EntityName>().ToString(),
+                Damage
+            ),
+            Constants.Colors.PlayerAttack
+        );
 
         postState = rng.GetState();
         rng.SetState(preState);
@@ -43,10 +50,26 @@ internal class MeleeAction : ActionWithDirection
     public override void Perform()
     {
         Twister.SetState(postState);
+        Entity entity = TargetEntity ?? default;
+        ref ResourceStat entHealth = ref entity.GetRelation<ResourceStat, ResourceID>(
+            ResourceID.Health
+        );
+        entHealth.Cur -= Damage;
+        if (entHealth.Cur <= 0)
+            entity.RemoveTag<IsAlive>();
+        base.Perform();
     }
 
     public override void Rewind()
     {
         Twister.SetState(preState);
+        Entity entity = TargetEntity ?? default;
+        ref ResourceStat entHealth = ref entity.GetRelation<ResourceStat, ResourceID>(
+            ResourceID.Health
+        );
+        entHealth.Cur += Damage;
+        if (entHealth.Cur > 0)
+            entity.AddTag<IsAlive>();
+        base.Rewind();
     }
 }
