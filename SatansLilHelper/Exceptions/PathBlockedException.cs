8c0d1c839598d0b0b2a298a926cef4c0ff52224c
elegantiron <128913032+elegantiron@users.noreply.github.com>
2025-03-24 09:48:33 -0400

    Create PathBlockedException.cs

---
+++ SatansLilHelper/Exceptions/PathBlockedException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SatansLilHelper.Exceptions;
+
+public class PathBlockedException : Exception
+{
+    public PathBlockedException()
+        : base() { }
+
+    public PathBlockedException(string message)
+        : base(message) { }
+}
