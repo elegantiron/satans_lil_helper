ffb10b0c05a067da6fae0b46b38f6964d70ccec4
elegantiron <128913032+elegantiron@users.noreply.github.com>
2025-03-25 11:02:25 -0400

    Create LogMessage.cs

---
+++ SatansLilHelper/Types/LogMessage.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace SatansLilHelper.Types;
+
+public struct LogMessage(string message, Color color, bool stack = true)
+{
+    public string Message = message;
+    public Color Color = color;
+    public bool Stack = stack;
+}
