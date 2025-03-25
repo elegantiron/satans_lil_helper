7e766bfce78ff99740a6bb7aebc10ac77e0ebaa6
elegantiron <128913032+elegantiron@users.noreply.github.com>
2025-03-25 11:02:28 -0400

    Create TextVecs.cs

---
+++ SatansLilHelper/Types/TextVecs.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace SatansLilHelper.Types;
+
+public struct TextVecs
+{
+    public Vector2 Location,
+        Origin;
+
+    public TextVecs()
+    {
+        Location = Origin = Vector2.Zero;
+    }
+}
