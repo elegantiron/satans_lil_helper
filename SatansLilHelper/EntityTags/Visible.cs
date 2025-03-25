046fcc4e41c613e76e1d02454e6fe28cb113896f
elegantiron <elegant.iron3167@fastmail.com>
2025-03-25 17:21:36 -0400

    Create Visible.cs

---
+++ SatansLilHelper/EntityTags/Visible.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Friflo.Engine.ECS;
+
+namespace SatansLilHelper.EntityTags;
+
+internal struct Visible : ITag { }
