a9058f9a6ca3caacc8fc707e58b75b04e92f87e1
elegantiron <128913032+elegantiron@users.noreply.github.com>
2025-03-25 15:14:28 -0400

    Create IMessageSender.cs

---
+++ SatansLilHelper/Interfaces/IMessageSender.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SatansLilHelper.Types;
+using SatansLilHelper.Utils;
+
+namespace SatansLilHelper.Interfaces;
+
+public interface IMessageSender
+{
+    List<LogMessage> Messages { get; }
+    void SendMessages()
+    {
+        foreach (LogMessage logMessage in Messages)
+            EventBus.Send(Events.AddLogMessage, logMessage);
+    }
+
+    void RetractMessages()
+    {
+        foreach (LogMessage logMessage in Messages.AsReadOnly().Reverse())
+            EventBus.Send(Events.PruneLogMessage, logMessage);
+    }
+}
