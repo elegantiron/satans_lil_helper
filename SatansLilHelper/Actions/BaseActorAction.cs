a065b764f1c9645ea4b4aa47089bf21d641e3984
elegantiron <128913032+elegantiron@users.noreply.github.com>
2025-03-25 14:05:49 -0400

    Create BaseActorAction.cs

---
+++ SatansLilHelper/Actions/BaseActorAction.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Friflo.Engine.ECS;
+using SatansLilHelper.Interfaces;
+using SatansLilHelper.Types;
+using SatansLilHelper.Utils;
+
+namespace SatansLilHelper.Actions;
+
+internal abstract class BaseActorAction(Entity entity, ArchetypeQuery query, bool isPlayer)
+    : IAction
+{
+    protected Entity Entity = entity;
+    protected bool IsPlayer = isPlayer;
+    protected ArchetypeQuery Query = query;
+    protected List<LogMessage> _logMessages = [];
+    protected List<BaseActorAction> _subActions = [];
+
+    public virtual void Perform()
+    {
+        if (_logMessages != null)
+            foreach (LogMessage logMessage in _logMessages)
+                EventBus.Send(Events.AddLogMessage, logMessage);
+    }
+
+    public virtual void Rewind()
+    {
+        if (_logMessages != null)
+            foreach (LogMessage logMessage in _logMessages.AsReadOnly().Reverse())
+                EventBus.Send(Events.PruneLogMessage, logMessage);
+    }
+}
