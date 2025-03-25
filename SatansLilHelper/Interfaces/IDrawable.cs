c6d2c0672cb5cd59b88a123d6f6226a4865e6a88
elegantiron <128913032+elegantiron@users.noreply.github.com>
2025-03-25 14:06:16 -0400

    Create IDrawable.cs

---
+++ SatansLilHelper/Interfaces/IDrawable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Media;
+using SatansLilHelper.Utils;
+
+namespace SatansLilHelper.Interfaces;
+
+internal interface IDrawable
+{
+    void Draw(
+        SpriteBatch spriteBatch,
+        Dictionary<TextureID, Texture2D> textureMap,
+        Dictionary<EffectID, SoundEffect> effectMap,
+        Dictionary<SongID, Song> songMap,
+        Dictionary<FontID, SpriteFont> fontMap
+    );
+}
