0f095c6e58b72846a9aeeb6611029324c28fc570
elegantiron <128913032+elegantiron@users.noreply.github.com>
2025-03-25 15:14:25 -0400

    Update IAction.cs

--- SatansLilHelper/Interfaces/IAction.cs
+++ SatansLilHelper/Interfaces/IAction.cs
@@ -1,7 +1,11 @@
-﻿namespace SatansLilHelper.Interfaces;
+﻿using Friflo.Engine.ECS;
+
+namespace SatansLilHelper.Interfaces;
 
 internal interface IAction
 {
     void Perform();
     void Rewind();
+
+    Entity Entity { get; }
 }
