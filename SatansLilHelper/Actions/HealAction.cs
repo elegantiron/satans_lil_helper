2469835e959001dd307bba6fd25204dd2b13e438
elegantiron <elegant.iron3167@fastmail.com>
2025-03-25 17:20:30 -0400

    Update HealAction.cs

--- SatansLilHelper/Actions/HealAction.cs
+++ SatansLilHelper/Actions/HealAction.cs
@@ -1,33 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
 using Friflo.Engine.ECS;
+using SatansLilHelper.Components;
+using SatansLilHelper.Content.Text;
 using SatansLilHelper.Interfaces;
 using SatansLilHelper.Types;
+using SatansLilHelper.Utils;
 
 namespace SatansLilHelper.Actions;
 
 #nullable enable
 internal class HealAction : IAction, IMessageSender
 {
-    private Entity _entity;
+    private Entity _target;
     private int _healAmount;
-    private List<LogMessage> _messages;
+    private List<LogMessage> _messages = [];
 
-    // nullable member fields
-    private byte[]? _preState,
-        _postState;
+    public HealAction(Entity target, int healAmount)
+    {
+        _target = target;
+        _healAmount = Math.Min(
+            healAmount,
+            EntityCalcs.GetStat(_target, ResourceID.Health)
+                - _target.GetRelation<ResourceStat, ResourceID>(ResourceID.Health).Cur
+        );
+        _messages.Add(
+            new LogMessage(
+                string.Format(GameStrings.HealPotionMessage, _healAmount),
+                Constants.Colors.PlayerHeal
+            )
+        );
+    }
+
+    public List<LogMessage> Messages
+    {
+        get { return _messages; }
+    }
     public Entity Entity
     {
-        get { return _entity; }
+        get { return _target; }
     }
 
     public void Perform()
     {
-        IMessageSender.SendMessages(_messages);
+        (this as IMessageSender).SendMessages();
+        _target.GetRelation<ResourceStat, ResourceID>(ResourceID.Health).Cur += _healAmount;
     }
 
     public void Rewind()
     {
-        RetractMessages(_messages);
+        (this as IMessageSender).RetractMessages();
+        _target.GetRelation<ResourceStat, ResourceID>(ResourceID.Health).Cur -= _healAmount;
     }
 }
