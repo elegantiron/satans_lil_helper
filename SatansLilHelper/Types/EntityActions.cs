454ca56083416929d3ce6e7ca8e1f10812134d1a
elegantiron <128913032+elegantiron@users.noreply.github.com>
2025-03-25 11:02:22 -0400

    Create EntityActions.cs

---
+++ SatansLilHelper/Types/EntityActions.cs
@@ -0,0 +1,10 @@
+﻿namespace SatansLilHelper.Types;
+
+public struct EntityActions(int moves = 0, int attacks = 0)
+{
+    public int Moves = moves;
+    public int Attacks = attacks;
+
+    public EntityActions()
+        : this(0, 0) { }
+}
