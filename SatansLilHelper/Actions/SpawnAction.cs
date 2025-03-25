9e366a16d9a29c0b03252737ff2fd97d9ea44eef
elegantiron <128913032+elegantiron@users.noreply.github.com>
2025-03-25 14:05:55 -0400

    Create SpawnAction.cs

---
+++ SatansLilHelper/Actions/SpawnAction.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Friflo.Engine.ECS;
+using SatansLilHelper.Interfaces;
+using SatansLilHelper.Utils;
+using SatansLilHelper.Utils.GameMaps;
+
+namespace SatansLilHelper.Actions;
+
+#nullable enable
+internal class SpawnAction : IAction
+{
+    private byte[]? preState = null,
+        postState = null;
+    private MersenneTwister? Rng = null;
+    private Entity Entity;
+
+    private SpawnAction(BaseMap gameMap)
+    {
+        Entity = gameMap.Registry.CreateEntity();
+    }
+
+    public SpawnAction(
+        Action<Entity, MersenneTwister> spawnFunction,
+        BaseMap gameMap,
+        MersenneTwister rng,
+        (int, int) location
+    )
+        : this(gameMap)
+    {
+        Rng = rng;
+        preState = Rng.GetState();
+        spawnFunction(Entity, Rng);
+        gameMap.PlaceEntity(Entity, location);
+        Entity.Enabled = false;
+    }
+
+    public SpawnAction(Action<Entity> spawnFunction, BaseMap gameMap)
+        : this(gameMap)
+    {
+        spawnFunction(Entity);
+        Entity.Enabled = false;
+    }
+
+    public void Perform()
+    {
+        Rng?.SetState(postState);
+        Entity.Enabled = true;
+    }
+
+    public void Rewind()
+    {
+        Rng?.SetState(preState);
+        Entity.Enabled = false;
+    }
+}
