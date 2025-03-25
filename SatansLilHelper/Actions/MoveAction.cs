190160b1b0f49075cc71bfef94d424c831f7025f
elegantiron <128913032+elegantiron@users.noreply.github.com>
2025-03-25 15:14:22 -0400

    Update MoveAction.cs

--- SatansLilHelper/Actions/MoveAction.cs
+++ SatansLilHelper/Actions/MoveAction.cs
@@ -1,15 +1,23 @@
-﻿using Friflo.Engine.ECS;
+﻿using System.Collections.Generic;
+using Friflo.Engine.ECS;
 using Microsoft.Xna.Framework;
 using SatansLilHelper.Components;
 using SatansLilHelper.Content.Text;
+using SatansLilHelper.Interfaces;
+using SatansLilHelper.Types;
 using SatansLilHelper.Utils;
 using SatansLilHelper.Utils.GameMaps;
 
 namespace SatansLilHelper.Actions;
 
 #nullable enable
-internal class MoveAction : ActionWithDirection
+internal class MoveAction : ActionWithDirection, IMessageSender
 {
+    public List<LogMessage> Messages
+    {
+        get { return _messages; }
+    }
+
     public MoveAction(
         Entity entity,
         Point direction,
@@ -21,22 +29,20 @@ internal class MoveAction : ActionWithDirection
     {
         if (IsBlocked)
             if (IsOffMap)
-                _logMessages.Add(new(GameStrings.MapEdge, Constants.Colors.Impossible));
+                _messages.Add(new(GameStrings.MapEdge, Constants.Colors.Impossible));
             else
-                _logMessages.Add(new(GameStrings.PathBlocked, Constants.Colors.Impossible));
+                _messages.Add(new(GameStrings.PathBlocked, Constants.Colors.Impossible));
     }
 
     public override void Perform()
     {
         if (!IsBlocked)
-            Entity.AddComponent<Location>(Destination);
-        base.Perform();
+            _entity.AddComponent<Location>(Destination);
     }
 
     public override void Rewind()
     {
         if (!IsBlocked)
-            Entity.AddComponent<Location>(Origin);
-        base.Rewind();
+            _entity.AddComponent<Location>(Origin);
     }
 }
