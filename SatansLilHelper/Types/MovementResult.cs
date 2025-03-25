89c4e8e2e94e3546dd2f6d28e17c420d6976ab21
elegantiron <128913032+elegantiron@users.noreply.github.com>
2025-03-25 11:02:27 -0400

    Create MovementResult.cs

---
+++ SatansLilHelper/Types/MovementResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Friflo.Engine.ECS;
+
+namespace SatansLilHelper.Types;
+
+public struct MovementResult(Entity entity, (int, int) origin, (int, int) destination)
+{
+    public Entity Entity = entity;
+    public (int X, int Y) Origin = origin,
+        Destination = destination;
+}
