e8efcc69f8f26063334a85bd2cf38278a33985c8
elegantiron <128913032+elegantiron@users.noreply.github.com>
2025-03-25 11:02:19 -0400

    Create ActionResult.cs

---
+++ SatansLilHelper/Types/ActionResult.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SatansLilHelper.Utils;
+
+namespace SatansLilHelper.Types;
+
+#nullable enable
+public struct ActionResult()
+{
+    public List<LogMessage>? Messages = null;
+    public MovementResult? MovementResult = null;
+    public List<EntityCreationResult>? EntityCreationResults = null;
+    public List<AttackResult>? AttackResults = null;
+
+    public ActionResult(MovementResult movementResult, List<LogMessage>? messages = null)
+        : this()
+    {
+        MovementResult = movementResult;
+        Messages = messages;
+    }
+}
