96c439662190e455e05a6498ab174624a2c0d79e
elegantiron <128913032+elegantiron@users.noreply.github.com>
2025-03-25 15:14:18 -0400

    Create HealAction.cs

---
+++ SatansLilHelper/Actions/HealAction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Friflo.Engine.ECS;
+using SatansLilHelper.Interfaces;
+using SatansLilHelper.Types;
+
+namespace SatansLilHelper.Actions;
+
+#nullable enable
+internal class HealAction : IAction, IMessageSender
+{
+    private Entity _entity;
+    private int _healAmount;
+    private List<LogMessage> _messages;
+
+    // nullable member fields
+    private byte[]? _preState,
+        _postState;
+    public Entity Entity
+    {
+        get { return _entity; }
+    }
+
+    public void Perform()
+    {
+        IMessageSender.SendMessages(_messages);
+    }
+
+    public void Rewind()
+    {
+        RetractMessages(_messages);
+    }
+}
