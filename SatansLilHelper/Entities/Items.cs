9267d70e2f6be2e7ca3ed69dbbc71bdb07fe56d7
elegantiron <elegant.iron3167@fastmail.com>
2025-03-25 17:21:22 -0400

    Create Items.cs

---
+++ SatansLilHelper/Entities/Items.cs
@@ -0,0 +1,21 @@
+﻿using Friflo.Engine.ECS;
+using SatansLilHelper.Components;
+using SatansLilHelper.EntityTags;
+using SatansLilHelper.Utils;
+
+namespace SatansLilHelper.Entities;
+
+public static class Items
+{
+    private static void Base(Entity entity)
+    {
+        entity.AddTag<Item>();
+    }
+
+    public static void HealthPotion(Entity entity, MersenneTwister rng)
+    {
+        Base(entity);
+        entity.AddComponent(new EntityName("Healing Potion"));
+        entity.AddComponent(new EffectPower(rng.Next(1, 8) + 2));
+    }
+}
