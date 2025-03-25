7f99c018701b4cffd2fab9586181f1990492d6d2
elegantiron <elegant.iron3167@fastmail.com>
2025-03-25 17:20:27 -0400

    Update ActionWithDirection.cs

--- SatansLilHelper/Actions/ActionWithDirection.cs
+++ SatansLilHelper/Actions/ActionWithDirection.cs
@@ -2,6 +2,7 @@
 using Friflo.Engine.ECS;
 using Microsoft.Xna.Framework;
 using SatansLilHelper.Components;
+using SatansLilHelper.EntityTags;
 using SatansLilHelper.Interfaces;
 using SatansLilHelper.Types;
 using SatansLilHelper.Utils.GameMaps;
@@ -12,30 +13,26 @@ internal abstract class ActionWithDirection : IAction
 {
     protected Location Destination,
         Origin;
-    protected bool IsBlocked = false,
-        IsOffMap = false;
+    protected bool _isBlocked = false,
+        _isOffMap = false,
+        _isPlayer;
     protected Entity? _target = null;
     protected Entity _entity;
-    protected List<LogMessage> _messages;
+    protected List<LogMessage> _messages = [];
 
-    public ActionWithDirection(
-        Entity entity,
-        Point direction,
-        BaseMap gameMap,
-        ArchetypeQuery query,
-        bool isPlayer
-    )
+    public ActionWithDirection(Entity entity, Point direction, BaseMap gameMap, bool isPlayer)
     {
+        _isPlayer = isPlayer;
         _entity = entity;
         Origin = entity.GetComponent<Location>();
         Destination = new Location(Origin.X + direction.X, Origin.Y + direction.Y);
         ArchetypeQuery Query = entity
             .Store.Query()
             .HasValue<Location, (int X, int Y)>((Destination.X, Destination.Y))
-            .AllTags(Tags.Get<IsAlive>());
+            .AllTags(Tags.Get<Alive>());
         if (Query.Count > 0)
         {
-            IsBlocked = true;
+            _isBlocked = true;
             foreach (Entity ent in Query.Entities)
             {
                 _target = ent;
@@ -49,16 +46,22 @@ internal abstract class ActionWithDirection : IAction
             || Destination.Y >= gameMap.Tiles.GetLength(1)
         )
         {
-            IsOffMap = true;
-            IsBlocked = true;
+            _isOffMap = true;
+            _isBlocked = true;
         }
-        if (!IsOffMap && !gameMap.Tiles[Destination.X, Destination.Y].Walkable)
-            IsBlocked = true;
+        if (!_isOffMap && !gameMap.Tiles[Destination.X, Destination.Y].Walkable)
+            _isBlocked = true;
     }
 
     public Entity Entity => throw new System.NotImplementedException();
 
-    public abstract void Perform();
+    public virtual void Perform()
+    {
+        (this as IMessageSender).SendMessages();
+    }
 
-    public abstract void Rewind();
+    public virtual void Rewind()
+    {
+        (this as IMessageSender).RetractMessages();
+    }
 }
