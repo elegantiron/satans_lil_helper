b66dacf29620737b526794b1ce0c90d10bad2762
elegantiron <128913032+elegantiron@users.noreply.github.com>
2025-03-25 14:06:14 -0400

    Create ICellGrid.cs

---
+++ SatansLilHelper/Interfaces/ICellGrid.cs
@@ -0,0 +1,36 @@
+﻿using Friflo.Engine.ECS;
+using Microsoft.Xna.Framework;
+using SatansLilHelper.Types;
+
+namespace SatansLilHelper.Interfaces;
+
+public interface ICellGrid
+{
+    bool IsPassable(Point tile)
+    {
+        return Tiles[tile.X, tile.Y].Walkable;
+    }
+    bool PassesLight(Point tile)
+    {
+        return Tiles[tile.X, tile.Y].PassesLight;
+    }
+    void SetLight(Point tile, float distanceSquared)
+    {
+        Tiles[tile.X, tile.Y].Visible = true;
+        Tiles[tile.X, tile.Y].Explored = true;
+        Tiles[tile.X, tile.Y].LightDistance = distanceSquared;
+    }
+    void GenerateMap(Point size);
+
+    int XDim
+    {
+        get { return Tiles.GetLength(0); }
+    }
+    int YDim
+    {
+        get { return Tiles.GetLength(1); }
+    }
+    Entity Player { get; }
+    EntityStore Registry { get; }
+    Tile[,] Tiles { get; }
+}
