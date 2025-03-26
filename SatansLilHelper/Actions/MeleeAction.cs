3cdcea4da1fc7d86afa2c8fdfce40c90f893d77c
elegantiron <elegant.iron3167@fastmail.com>
2025-03-26 18:14:01 -0400

    Update MeleeAction.cs

--- SatansLilHelper/Actions/MeleeAction.cs
+++ SatansLilHelper/Actions/MeleeAction.cs
@@ -12,12 +12,13 @@ using SatansLilHelper.Utils.GameMaps;
 
 namespace SatansLilHelper.Actions;
 
+#nullable enable
 internal class MeleeAction : ActionWithDirection, IMessageSender
 {
     protected byte[] preState,
         postState;
     protected MersenneTwister Twister;
-    protected int Damage;
+    protected uint Damage;
     protected bool IsKill;
     protected string TargetName;
     public List<LogMessage> Messages
@@ -34,59 +35,65 @@ internal class MeleeAction : ActionWithDirection, IMessageSender
     )
         : base(entity, direction, gameMap, isPlayer)
     {
-        if (!_isBlocked || _target == null)
-            throw new MissingTargetException();
-        TargetName = _target?.GetComponent<EntityName>().value;
-        Twister = rng;
-        preState = rng.GetState();
-
-        Damage = EntityCalcs.GetDamage(Twister, _entity, (Entity)_target);
-        _messages.Add(
-            new(
-                string.Format(GameStrings.PlayerAttack, TargetName, Damage),
-                Constants.Colors.PlayerAttack
-            )
-        );
-        if (Damage >= _target?.GetRelation<ResourceStat, ResourceID>(ResourceID.Health).Cur)
+        if (_isBlocked && _target is Entity targetEnt)
         {
-            IsKill = true;
+            TargetName = targetEnt.GetComponent<EntityName>().value;
+            Twister = rng;
+            preState = Twister.GetState();
+
+            Damage = EntityCalcs.GetDamage(Twister, _entity, targetEnt);
             _messages.Add(
                 new(
-                    string.Format(GameStrings.EnemyDeath, TargetName),
+                    string.Format(GameStrings.PlayerAttack, TargetName, Damage),
                     Constants.Colors.PlayerAttack
                 )
             );
-            _messages.Add(
-                new(string.Format(GameStrings.GainExperience, 5), Constants.Colors.AmericanRose)
-            );
+            if (Damage >= targetEnt.GetRelation<ResourceStat, ResourceID>(ResourceID.Health).Cur)
+            {
+                IsKill = true;
+                _messages.Add(
+                    new(
+                        string.Format(GameStrings.EnemyDeath, TargetName),
+                        Constants.Colors.PlayerAttack
+                    )
+                );
+                _messages.Add(
+                    new(string.Format(GameStrings.GainExperience, 5), Constants.Colors.AmericanRose)
+                );
+            }
+            postState = Twister.GetState();
         }
-        postState = rng.GetState();
-        rng.SetState(preState);
+        else
+            throw new MissingTargetException();
     }
 
     public override void Perform()
     {
-        base.Perform();
-        Twister.SetState(postState);
-        Entity entity = _target ?? default;
-        ref ResourceStat entHealth = ref entity.GetRelation<ResourceStat, ResourceID>(
-            ResourceID.Health
-        );
-        entHealth.Cur -= Damage;
-        if (IsKill)
-            entity.RemoveTag<Alive>();
+        if (_target is Entity entity)
+        {
+            ref ResourceStat entHealth = ref entity.GetRelation<ResourceStat, ResourceID>(
+                ResourceID.Health
+            );
+            entHealth.Cur -= Damage;
+            if (IsKill)
+                entity.RemoveTag<Alive>();
+            Twister.SetState(postState);
+            base.Perform();
+        }
     }
 
     public override void Rewind()
     {
-        base.Rewind();
-        Twister.SetState(preState);
-        Entity entity = _target ?? default;
-        ref ResourceStat entHealth = ref entity.GetRelation<ResourceStat, ResourceID>(
-            ResourceID.Health
-        );
-        entHealth.Cur += Damage;
-        if (IsKill)
-            entity.AddTag<Alive>();
+        if (_target is Entity entity)
+        {
+            ref ResourceStat entHealth = ref entity.GetRelation<ResourceStat, ResourceID>(
+                ResourceID.Health
+            );
+            entHealth.Cur += Damage;
+            if (IsKill)
+                entity.AddTag<Alive>();
+            Twister.SetState(preState);
+            base.Rewind();
+        }
     }
 }
