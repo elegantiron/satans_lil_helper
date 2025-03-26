e4d7f739d3ec17dd286cd24c8d7dac504da90141
elegantiron <elegant.iron3167@fastmail.com>
2025-03-26 18:14:20 -0400

    Create TextureIndex.cs

---
+++ SatansLilHelper/Components/TextureIndex.cs
@@ -0,0 +1,20 @@
+﻿using Friflo.Engine.ECS;
+using SatansLilHelper.Utils;
+
+namespace SatansLilHelper.Components;
+
+[ComponentKey("texture-index")]
+public struct TextureIndex : IComponent
+{
+    public TextureID Index;
+
+    public TextureIndex()
+    {
+        Index = TextureID.Missing;
+    }
+
+    public TextureIndex(TextureID index)
+    {
+        Index = index;
+    }
+}
