8c83bd576b97de7f3c411845434a3856e4caffad
elegantiron <elegant.iron3167@fastmail.com>
2025-03-26 18:14:10 -0400

    Create Attack.cs

---
+++ SatansLilHelper/Components/Attack.cs
@@ -0,0 +1,11 @@
+﻿using Friflo.Engine.ECS;
+
+namespace SatansLilHelper.Components;
+
+[ComponentKey("attack")]
+public struct Attack(uint dice, uint sides, uint bonus = 0) : IComponent
+{
+    public uint Dice = dice,
+        Sides = sides,
+        Bonus = bonus;
+}
