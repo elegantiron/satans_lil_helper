4edaa593c3165589d4a6430c8da868649f73dc85
elegantiron <elegant.iron3167@fastmail.com>
2025-03-26 18:13:58 -0400

    Create FullHealAction.cs

---
+++ SatansLilHelper/Actions/FullHealAction.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Friflo.Engine.ECS;
+using SatansLilHelper.Components;
+using SatansLilHelper.Content.Text;
+using SatansLilHelper.Interfaces;
+using SatansLilHelper.Types;
+using SatansLilHelper.Utils;
+
+namespace SatansLilHelper.Actions;
+
+#nullable enable
+internal class FullHealAction : IAction, IMessageSender
+{
+    private Entity _target;
+    private uint _healAmount;
+    private List<LogMessage> _messages = [];
+
+    public FullHealAction(Entity target)
+    {
+        _target = target;
+        _healAmount =
+            (uint)EntityCalcs.GetStat(_target, ResourceID.Health)
+            - _target.GetRelation<ResourceStat, ResourceID>(ResourceID.Health).Cur;
+        _messages.Add(new LogMessage(GameStrings.FullHeal, Constants.Colors.PlayerHeal));
+    }
+
+    public List<LogMessage> Messages
+    {
+        get { return _messages; }
+    }
+    public Entity? Entity
+    {
+        get { return _target; }
+    }
+
+    public void Perform()
+    {
+        (this as IMessageSender).SendMessages();
+        _target.GetRelation<ResourceStat, ResourceID>(ResourceID.Health).Cur += _healAmount;
+    }
+
+    public void Rewind()
+    {
+        (this as IMessageSender).RetractMessages();
+        _target.GetRelation<ResourceStat, ResourceID>(ResourceID.Health).Cur -= _healAmount;
+    }
+}
