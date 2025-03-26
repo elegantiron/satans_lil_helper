bc9a54f33835964fe66a802cb7272c5c5e5517d1
elegantiron <elegant.iron3167@fastmail.com>
2025-03-26 18:14:13 -0400

    Create EquippedItem.cs

---
+++ SatansLilHelper/Components/EquippedItem.cs
@@ -0,0 +1,10 @@
+﻿using Friflo.Engine.ECS;
+
+namespace SatansLilHelper.Components;
+
+internal struct Equipper(Entity target) : ILinkComponent
+{
+    public Entity Target = target;
+
+    public readonly Entity GetIndexedValue() => Target;
+}
