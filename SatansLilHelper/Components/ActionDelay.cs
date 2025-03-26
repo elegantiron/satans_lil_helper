34083844fb23ee0cdd5f08e0818d02b1d6d4b2b1
elegantiron <elegant.iron3167@fastmail.com>
2025-03-26 18:14:08 -0400

    Create ActionDelay.cs

---
+++ SatansLilHelper/Components/ActionDelay.cs
@@ -0,0 +1,9 @@
+﻿using Friflo.Engine.ECS;
+
+namespace SatansLilHelper.Components;
+
+[ComponentKey("action-delay")]
+public struct ActionDelay(uint value) : IComponent
+{
+    public uint Value = value;
+}
