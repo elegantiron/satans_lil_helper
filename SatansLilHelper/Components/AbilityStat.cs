c33a4c13d2741f9f9e197d24c9259ac571acf6c8
elegantiron <elegant.iron3167@fastmail.com>
2025-03-26 18:14:06 -0400

    Create AbilityStat.cs

---
+++ SatansLilHelper/Components/AbilityStat.cs
@@ -0,0 +1,16 @@
+﻿using Friflo.Engine.ECS;
+using SatansLilHelper.Utils;
+
+namespace SatansLilHelper.Components;
+
+public struct AbilityStat(AbilityID type, decimal basis, decimal growth) : IRelation<AbilityID>
+{
+    public AbilityID Type = type;
+    public decimal Basis = basis,
+        Growth = growth;
+
+    public AbilityStat(AbilityID type, decimal basis)
+        : this(type, basis, 0m) { }
+
+    public readonly AbilityID GetRelationKey() => Type;
+}
