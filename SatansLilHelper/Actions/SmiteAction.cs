215c358323a623498ffa7de1b75d350ff9784268
elegantiron <elegant.iron3167@fastmail.com>
2025-03-26 18:14:02 -0400

    Create SmiteAction.cs

---
+++ SatansLilHelper/Actions/SmiteAction.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Friflo.Engine.ECS;
+using SatansLilHelper.Components;
+using SatansLilHelper.Content.Text;
+using SatansLilHelper.Interfaces;
+using SatansLilHelper.Types;
+using SatansLilHelper.Utils;
+
+namespace SatansLilHelper.Actions;
+
+internal class SmiteAction(Entity entity) : IAction, IMessageSender
+{
+    private List<LogMessage> _messages =
+    [
+        new LogMessage(GameStrings.DebugSmite, Constants.Colors.EnemyAttack),
+    ];
+    private Entity _entity = entity;
+
+    public List<LogMessage> Messages
+    {
+        get { return _messages; }
+    }
+    public Entity? Entity
+    {
+        get { return _entity; }
+    }
+
+    public void Perform()
+    {
+        (this as IMessageSender).SendMessages();
+        _entity.GetRelation<ResourceStat, ResourceID>(ResourceID.Health).Cur--;
+    }
+
+    public void Rewind()
+    {
+        (this as IMessageSender).RetractMessages();
+        _entity.GetRelation<ResourceStat, ResourceID>(ResourceID.Health).Cur++;
+    }
+}
