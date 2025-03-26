fc64b2ae2bc559b2a894725b8d729fecf5ea5e6b
elegantiron <elegant.iron3167@fastmail.com>
2025-03-26 18:14:58 -0400

    Create IRandom.cs

---
+++ SatansLilHelper/Interfaces/IRandom.cs
@@ -0,0 +1,9 @@
+﻿namespace SatansLilHelper.Interfaces;
+
+internal interface IRandom
+{
+    void SetState(byte[] data);
+    byte[] GetState();
+    uint Next(uint maxValue);
+    uint Next(uint minValue, uint maxValue);
+}
