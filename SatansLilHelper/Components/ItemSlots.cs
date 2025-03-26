d0022929ebd7e4a9c82ea45df2936b11f2cbf1a4
elegantiron <elegant.iron3167@fastmail.com>
2025-03-26 18:14:14 -0400

    Create ItemSlots.cs

---
+++ SatansLilHelper/Components/ItemSlots.cs
@@ -0,0 +1,10 @@
+﻿using Friflo.Engine.ECS;
+using SatansLilHelper.Utils;
+
+namespace SatansLilHelper.Components;
+
+[ComponentKey("item-slots")]
+public struct ItemSlots(ItemType types) : IComponent
+{
+    public ItemType Types = types;
+}
