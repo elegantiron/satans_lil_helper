3cc0c5a43a823f314a4c63a3a1fb734c3c133a62
elegantiron <elegant.iron3167@fastmail.com>
2025-03-26 18:14:17 -0400

    Create Location.cs

---
+++ SatansLilHelper/Components/Location.cs
@@ -0,0 +1,15 @@
+﻿using Friflo.Engine.ECS;
+
+namespace SatansLilHelper.Components;
+
+[ComponentKey("location")]
+public struct Location(int x, int y) : IIndexedComponent<(int, int)>
+{
+    public int X = x,
+        Y = y;
+
+    public readonly (int, int) GetIndexedValue()
+    {
+        return (X, Y);
+    }
+}
