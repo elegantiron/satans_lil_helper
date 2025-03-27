fa8add01a88fcface039510a821873506012fd7f
elegantiron <128913032+elegantiron@users.noreply.github.com>
2025-03-27 14:53:44 -0400

    Create IKill.cs

---
+++ SatansLilHelper/Interfaces/IKill.cs
@@ -0,0 +1,9 @@
+﻿namespace SatansLilHelper.Interfaces;
+
+internal interface IKill
+{
+    int Alpha { get; }
+    int Normal { get; }
+    void AddKill(bool isAlpha);
+    void RemoveKill(bool isAlpha);
+}
