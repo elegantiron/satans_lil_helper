3242a0b0f22a16b5827e42cc63044327243e3908
elegantiron <elegant.iron3167@fastmail.com>
2025-03-28 17:38:50 -0400

    Create ItemType.cs

---
+++ SatansLilHelper/Constants/ItemType.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SatansLilHelper.Constants;
+
+public enum ItemType
+{
+    // Base item types
+    None = 0 << 0,
+    Weapon1H = 1 << 0,
+    Shield = 1 << 1,
+    Armor = 1 << 2,
+    Helm = 1 << 3,
+    Gauntlets = 1 << 4,
+    Boots = 1 << 5,
+    RingL = 1 << 6,
+    RingR = 1 << 7,
+    Amulet = 1 << 8,
+
+    // Common combinations
+    Base = RingL | RingR | Amulet,
+    Warrior = Base | Weapon1H | Shield | Armor | Helm | Gauntlets | Boots,
+}
