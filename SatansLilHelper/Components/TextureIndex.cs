67a4c06688ab84c2dff29c21774e7cd801e5b1dd
elegantiron <elegant.iron3167@fastmail.com>
2025-03-28 17:38:40 -0400

    Update TextureIndex.cs

--- SatansLilHelper/Components/TextureIndex.cs
+++ SatansLilHelper/Components/TextureIndex.cs
@@ -1,4 +1,6 @@
 ﻿using Friflo.Engine.ECS;
+using Friflo.Json.Fliox;
+using SatansLilHelper.Constants;
 using SatansLilHelper.Utils;
 
 namespace SatansLilHelper.Components;
@@ -6,6 +8,7 @@ namespace SatansLilHelper.Components;
 [ComponentKey("texture-index")]
 public struct TextureIndex : IComponent
 {
+    [Serialize]
     public TextureID Index;
 
     public TextureIndex()
