c672d0099a9c1bd6d8ca13a526761290498d0d24
elegantiron <elegant.iron3167@fastmail.com>
2025-03-28 17:38:26 -0400

    Update ActionDelay.cs

--- SatansLilHelper/Components/ActionDelay.cs
+++ SatansLilHelper/Components/ActionDelay.cs
@@ -1,9 +1,11 @@
 ﻿using Friflo.Engine.ECS;
+using Friflo.Json.Fliox;
 
 namespace SatansLilHelper.Components;
 
 [ComponentKey("action-delay")]
 public struct ActionDelay(uint value) : IComponent
 {
+    [Serialize]
     public uint Value = value;
 }
