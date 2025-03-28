d0acefd54b9744f933ba6a7fc64f01ffcb6ce768
elegantiron <elegant.iron3167@fastmail.com>
2025-03-28 17:38:54 -0400

    Create TextureID.cs

---
+++ SatansLilHelper/Constants/TextureID.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SatansLilHelper.Constants;
+
+public enum TextureID
+{
+    Missing,
+    Player,
+    ForestFloor,
+    ForestWall,
+    SatanMain,
+    SatanEyesOpen,
+    SatanEyesClosed,
+    SatanMouthOpen,
+    SatanMouthClosed,
+    WhitePixel,
+    Orc,
+    Wolf,
+}
