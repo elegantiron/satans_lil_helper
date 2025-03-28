863c646433ad2d64f6c4f1e9a2c7a1e13b82d201
elegantiron <elegant.iron3167@fastmail.com>
2025-03-28 17:38:36 -0400

    Update Location.cs

--- SatansLilHelper/Components/Location.cs
+++ SatansLilHelper/Components/Location.cs
@@ -1,10 +1,12 @@
 ﻿using Friflo.Engine.ECS;
+using Friflo.Json.Fliox;
 
 namespace SatansLilHelper.Components;
 
 [ComponentKey("location")]
 public struct Location(int x, int y) : IIndexedComponent<(int, int)>
 {
+    [Serialize]
     public int X = x,
         Y = y;
 
