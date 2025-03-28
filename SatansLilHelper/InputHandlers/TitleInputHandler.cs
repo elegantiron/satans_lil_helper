8c101a48df6d9189bce9df81668db634d5350bd4
elegantiron <elegant.iron3167@fastmail.com>
2025-03-28 17:39:09 -0400

    Update TitleInputHandler.cs

--- SatansLilHelper/InputHandlers/TitleInputHandler.cs
+++ SatansLilHelper/InputHandlers/TitleInputHandler.cs
@@ -4,6 +4,7 @@ using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
+using SatansLilHelper.Constants;
 using SatansLilHelper.Interfaces;
 using SatansLilHelper.Properties;
 using SatansLilHelper.Types;
