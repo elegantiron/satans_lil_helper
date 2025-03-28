1cdaed45860e842fbeb89a8ec36d83b2fb872715
elegantiron <elegant.iron3167@fastmail.com>
2025-03-28 17:38:34 -0400

    Update Level.cs

--- SatansLilHelper/Components/Level.cs
+++ SatansLilHelper/Components/Level.cs
@@ -1,9 +1,11 @@
 ﻿using Friflo.Engine.ECS;
+using Friflo.Json.Fliox;
 
 namespace SatansLilHelper.Components;
 
 [ComponentKey("level")]
 public struct Level(uint value) : IComponent
 {
+    [Serialize]
     public uint Value = value;
 }
