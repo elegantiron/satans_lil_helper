51a08d5d67f9bab5b1963e83d02c496195bae1ec
elegantiron <elegant.iron3167@fastmail.com>
2025-03-28 17:38:33 -0400

    Update ItemSlots.cs

--- SatansLilHelper/Components/ItemSlots.cs
+++ SatansLilHelper/Components/ItemSlots.cs
@@ -1,4 +1,6 @@
 ﻿using Friflo.Engine.ECS;
+using Friflo.Json.Fliox;
+using SatansLilHelper.Constants;
 using SatansLilHelper.Utils;
 
 namespace SatansLilHelper.Components;
@@ -6,5 +8,6 @@ namespace SatansLilHelper.Components;
 [ComponentKey("item-slots")]
 public struct ItemSlots(ItemType types) : IComponent
 {
+    [Serialize]
     public ItemType Types = types;
 }
