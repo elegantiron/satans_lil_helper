55cdc02ccf46db9f1abc7fef3bd2819ffc41aa83
elegantiron <elegant.iron3167@fastmail.com>
2025-03-28 17:39:14 -0400

    Create ISaveable.cs

---
+++ SatansLilHelper/Interfaces/ISaveable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SatansLilHelper.Interfaces;
+
+internal interface ISaveable
+{
+    void DumpData(string path);
+}
