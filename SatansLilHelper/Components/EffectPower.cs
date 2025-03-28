d6ae705929f5c1c99b2be38934cdbaaa599efe16
elegantiron <elegant.iron3167@fastmail.com>
2025-03-28 17:38:29 -0400

    Update EffectPower.cs

--- SatansLilHelper/Components/EffectPower.cs
+++ SatansLilHelper/Components/EffectPower.cs
@@ -1,8 +1,11 @@
 ﻿using Friflo.Engine.ECS;
+using Friflo.Json.Fliox;
 
 namespace SatansLilHelper.Components;
 
+[ComponentKey("effect-power")]
 internal struct EffectPower(int value) : IComponent
 {
+    [Serialize]
     public int Value = value;
 }
