f6dc87cff46c41878a0fe2767029069ddbf47217
elegantiron <elegant.iron3167@fastmail.com>
2025-03-28 17:38:23 -0400

    Update AbilityStat.cs

--- SatansLilHelper/Components/AbilityStat.cs
+++ SatansLilHelper/Components/AbilityStat.cs
@@ -1,13 +1,23 @@
 ﻿using Friflo.Engine.ECS;
-using SatansLilHelper.Utils;
+using Friflo.Json.Fliox;
+using SatansLilHelper.Constants;
 
 namespace SatansLilHelper.Components;
 
+[ComponentKey("ability-stat")]
 public struct AbilityStat(AbilityID type, decimal basis, decimal growth) : IRelation<AbilityID>
 {
+    [Serialize]
     public AbilityID Type = type;
-    public decimal Basis = basis,
-        Growth = growth;
+
+    [Serialize]
+    public decimal Basis = basis;
+
+    [Serialize]
+    public decimal Growth = growth;
+
+    [Serialize]
+    public uint Cur = (uint)basis;
 
     public AbilityStat(AbilityID type, decimal basis)
         : this(type, basis, 0m) { }
