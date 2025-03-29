ba7df6a0dbc72e7e0e5d6b5df8293ad9391cb39a
elegantiron <elegant.iron3167@fastmail.com>
2025-03-29 13:09:18 -0400

    Update IRandom.cs

--- SatansLilHelper/Interfaces/IRandom.cs
+++ SatansLilHelper/Interfaces/IRandom.cs
@@ -5,6 +5,8 @@ internal interface IRandom
     void SetState(byte[] data);
     byte[] GetState();
     uint Next();
+    void Seed(uint seed);
+    void Seed(int seed);
     uint Next(uint maxValue);
     uint Next(uint minValue, uint maxValue);
     double NextDouble();
