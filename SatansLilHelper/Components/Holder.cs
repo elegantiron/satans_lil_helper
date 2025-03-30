054e2bc2867ad4e207f922690f3ea0385354295c
elegantiron <elegant.iron3167@fastmail.com>
2025-03-29 20:53:48 -0400

    Create Holder.cs

---
+++ SatansLilHelper/Components/Holder.cs
@@ -0,0 +1,13 @@
+﻿using Friflo.Engine.ECS;
+using Friflo.Json.Fliox;
+
+namespace SatansLilHelper.Components;
+
+[ComponentKey("holder")]
+internal struct Holder(Entity target) : ILinkComponent
+{
+    [Serialize]
+    public Entity Target = target;
+
+    public readonly Entity GetIndexedValue() => Target;
+}
