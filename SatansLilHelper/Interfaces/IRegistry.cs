927880306443b6d4fc03c10cab94565b76e10287
elegantiron <elegant.iron3167@fastmail.com>
2025-03-30 15:50:23 -0400

    Update IRegistry.cs

--- SatansLilHelper/Interfaces/IRegistry.cs
+++ SatansLilHelper/Interfaces/IRegistry.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using Friflo.Engine.ECS;
 using Microsoft.Xna.Framework;
+using SatansLilHelper.Actions;
 using SatansLilHelper.Components;
 using SatansLilHelper.Constants;
+using SatansLilHelper.Types;
 using SatansLilHelper.Utils;
 using SatansLilHelper.Utils.GameMaps;
 
@@ -13,6 +16,7 @@ internal interface IRegistry : IUpdateable
 {
     BaseMap CurrentMap { get; }
     Entity Player { get; }
+    IRandom Generator { get; }
     bool IsPlayerNext { get; }
     Entity GetNextActor();
     void IUpdateable.Update(GameTime gameTime)
@@ -21,17 +25,40 @@ internal interface IRegistry : IUpdateable
         while (!IsPlayerNext)
         {
             Entity entity = GetNextActor();
-            Location entLoc = entity.GetComponent<Location>();
-            Point entPos = new(entLoc.X, entLoc.Y);
-            int radius = Math.Min(
-                EntityCalcs.GetStat(entity, AbilityID.Vision),
-                EntityCalcs.GetStat(entity, AbilityID.LightRadius)
-            );
+            EntityTurn turn = new(entity);
+            while (turn.HasActions)
+            {
+                Location entLoc = entity.GetComponent<Location>();
+                Point entPos = new(entLoc.X, entLoc.Y);
+                int radius = Math.Min(
+                    EntityCalcs.GetStat(entity, AbilityID.Vision),
+                    EntityCalcs.GetStat(entity, AbilityID.LightRadius)
+                );
 
-            List<Point> visibleTiles = ShadowCast.GetVisibleTiles(CurrentMap, entPos, radius);
+                List<Point> visibleTiles = ShadowCast.GetVisibleTiles(CurrentMap, entPos, radius);
+                foreach (var point in visibleTiles)
+                {
+                    Debug.WriteLine($"{point.X} {point.Y}");
+                }
 
-            if (visibleTiles.Contains(new Point(playerLoc.X, playerLoc.Y))) { }
-            else { }
+                if (visibleTiles.Exists(test => test.X == playerLoc.X && test.Y == playerLoc.Y))
+                {
+                    Debug.WriteLine("I see the player");
+                }
+                else
+                {
+                    if (turn.HasMoves)
+                    {
+                        int x = Generator.NextDouble() < 0.5 ? 1 : 0;
+                        x *= Generator.NextDouble() < 0.5 ? -1 : 1;
+                        int y = Generator.NextDouble() < 0.5 ? 1 : 0;
+                        y *= Generator.NextDouble() < 0.5 ? -1 : 1;
+                        turn.AddAction(new MoveAction(entity, new Point(x, y), CurrentMap, false));
+                    }
+                    else
+                        break;
+                }
+            }
         }
     }
 }
