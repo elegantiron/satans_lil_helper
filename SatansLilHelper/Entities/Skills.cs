b09f1bded2de13605d029b362c06c521d51d38a6
elegantiron <elegant.iron3167@fastmail.com>
2025-03-30 15:50:17 -0400

    Create Skills.cs

---
+++ SatansLilHelper/Entities/Skills.cs
@@ -0,0 +1,42 @@
+﻿using Friflo.Engine.ECS;
+using SatansLilHelper.Components;
+using SatansLilHelper.EntityTags;
+
+namespace SatansLilHelper.Entities;
+
+internal static class Skills
+{
+    private static void Base(Entity entity)
+    {
+        entity.AddTag<Skill>();
+    }
+
+    private static void Base(Entity entity, Entity parent)
+    {
+        entity.AddComponent(new KnownBy(parent));
+    }
+
+    public static void ShieldUp(Entity entity)
+    {
+        Base(entity);
+        entity.AddComponent(new EntityName("Shield Up"));
+    }
+
+    public static void ShieldUp(Entity entity, Entity parent)
+    {
+        Base(entity, parent);
+        ShieldUp(entity);
+    }
+
+    public static void Charge(Entity entity)
+    {
+        Base(entity);
+        entity.AddComponent(new EntityName("Charge"));
+    }
+
+    public static void Charge(Entity entity, Entity parent)
+    {
+        Base(entity, parent);
+        Charge(entity);
+    }
+}
