1259645650a9e61d4eaf51fc59b3d3ffce86c4fb
elegantiron <elegant.iron3167@fastmail.com>
2025-03-29 20:53:46 -0400

    Create Equipper.cs

---
+++ SatansLilHelper/Components/Equipper.cs
@@ -0,0 +1,13 @@
+﻿using Friflo.Engine.ECS;
+using Friflo.Json.Fliox;
+
+namespace SatansLilHelper.Components;
+
+[ComponentKey("equipper")]
+internal struct Equipper(Entity target) : ILinkComponent
+{
+    [Serialize]
+    public Entity Target = target;
+
+    public readonly Entity GetIndexedValue() => Target;
+}
