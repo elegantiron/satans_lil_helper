1c837209245f966eaba6540845223e46c3955c40
elegantiron <elegant.iron3167@fastmail.com>
2025-03-30 15:50:04 -0400

    Create KnownBy.cs

---
+++ SatansLilHelper/Components/KnownBy.cs
@@ -0,0 +1,13 @@
+﻿using Friflo.Engine.ECS;
+using Friflo.Json.Fliox;
+
+namespace SatansLilHelper.Components;
+
+[ComponentKey("known-by")]
+internal struct KnownBy(Entity target) : ILinkComponent
+{
+    [Serialize]
+    public Entity Target = target;
+
+    public readonly Entity GetIndexedValue() => Target;
+}
