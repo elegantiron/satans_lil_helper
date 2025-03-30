2dc6d41aaf5680bdd90ea3f2e7cfc1834e66d7e8
elegantiron <elegant.iron3167@fastmail.com>
2025-03-29 20:53:50 -0400

    Update Items.cs

--- SatansLilHelper/Entities/Items.cs
+++ SatansLilHelper/Entities/Items.cs
@@ -2,27 +2,29 @@
 using SatansLilHelper.Components;
 using SatansLilHelper.Constants;
 using SatansLilHelper.EntityTags;
+using SatansLilHelper.Interfaces;
 using SatansLilHelper.Utils;
 
 namespace SatansLilHelper.Entities;
 
-public static class Items
+internal static class Items
 {
     private static void Base(Entity entity)
     {
         entity.AddTag<Item>();
     }
 
-    public static void HealthPotion(Entity entity, MersenneTwister rng)
+    public static void HealthPotion(Entity entity, IRandom rng)
     {
         Base(entity);
         entity.AddComponent(new EntityName("Healing Potion"));
-        entity.AddComponent(new EffectPower(rng.Next(1, 8) + 2));
+        entity.AddComponent(new EffectPower((int)rng.Next(1, 8) + 2));
     }
 
     public static void Torch(Entity entity)
     {
         Base(entity);
         entity.AddRelation(new AbilityStat(AbilityID.LightRadius, 6m));
+        entity.AddComponent(new EntityName("Torch"));
     }
 }
