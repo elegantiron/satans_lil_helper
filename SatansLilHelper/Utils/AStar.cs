4ff77e6973f6fc643c34339a7e279e09c5c7a843
elegantiron <elegant.iron3167@fastmail.com>
2025-03-31 17:01:15 -0400

    Create AStar.cs

---
+++ SatansLilHelper/Utils/AStar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using SatansLilHelper.Interfaces;
+
+namespace SatansLilHelper.Utils;
+
+internal static partial class Pathfinders
+{
+    public static List<(int, int)> AStar(Point start, Point end, ICellGrid gameMap)
+    {
+        throw new NotImplementedException();
+    }
+}
