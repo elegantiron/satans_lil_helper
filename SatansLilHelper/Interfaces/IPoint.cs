2ec06cfd0a9290653eac5ecf31a8a4b96d786c17
elegantiron <elegant.iron3167@fastmail.com>
2025-03-31 18:09:49 -0400

    Create IPoint.cs

---
+++ SatansLilHelper/Interfaces/IPoint.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SatansLilHelper.Interfaces;
+
+internal interface IPoint
+{
+    int X { get; }
+    int Y { get; }
+}
