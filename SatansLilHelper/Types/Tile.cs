9799276bc90309cf2fc54338d6301144403c4412
elegantiron <elegant.iron3167@fastmail.com>
2025-03-31 15:57:49 -0400

    Update Tile.cs

--- SatansLilHelper/Types/Tile.cs
+++ SatansLilHelper/Types/Tile.cs
@@ -11,4 +11,5 @@ public struct Tile(TextureID texture, bool walkable, bool passesLight)
     public bool Visible = false;
     public bool Explored = false;
     public float LightDistance = 0f;
+    public int MovementCost = int.MinValue;
 }
