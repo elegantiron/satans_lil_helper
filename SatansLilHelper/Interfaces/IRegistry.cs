5b24241a2fb79c3c0f075719147f2be0b9414d63
elegantiron <elegant.iron3167@fastmail.com>
2025-04-01 16:40:58 -0400

    Update IRegistry.cs

--- SatansLilHelper/Interfaces/IRegistry.cs
+++ SatansLilHelper/Interfaces/IRegistry.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using Friflo.Engine.ECS;
+﻿using Friflo.Engine.ECS;
 using Microsoft.Xna.Framework;
-using SatansLilHelper.Actions;
+using SatansLilHelper.AI;
 using SatansLilHelper.Components;
-using SatansLilHelper.Constants;
-using SatansLilHelper.Types;
 using SatansLilHelper.Utils;
 using SatansLilHelper.Utils.GameMaps;
 
@@ -26,70 +21,7 @@ internal interface IRegistry
         while (!IsPlayerNext)
         {
             Entity entity = GetNextActor();
-            EntityTurn turn = new(entity);
-            while (turn.HasActions)
-            {
-                Location entLoc = entity.GetComponent<Location>();
-                Point entPos = new(entLoc.X, entLoc.Y);
-                int radius = Math.Min(
-                    EntityCalcs.GetStat(entity, AbilityID.Vision),
-                    EntityCalcs.GetStat(entity, AbilityID.LightRadius)
-                );
-
-                List<Point> visibleTiles = ShadowCast.GetVisibleTiles(CurrentMap, entPos, radius);
-                List<(int X, int Y)> path;
-
-                if (visibleTiles.Exists(test => test.X == playerLoc.X && test.Y == playerLoc.Y))
-                {
-                    path = Pathfinders.Dijkstra(entPos, (playerLoc.X, playerLoc.Y), CurrentMap);
-                    Debug.WriteLine($"{path[0].X} {path[0].Y}; {entPos.X} {entPos.Y}");
-                    if (turn.HasSwift && false)
-                    {
-                        // Do swift action
-                    }
-                    else if (path.Count > 1 && turn.HasMoves)
-                    {
-                        turn.AddAction(
-                            new MoveAction(
-                                entity,
-                                new Point(path[0].X - entPos.X, path[0].Y - entPos.Y),
-                                CurrentMap,
-                                false
-                            )
-                        );
-                    }
-                    else if (path.Count == 1 && turn.HasAttacks)
-                    {
-                        //turn.AddAction(
-                        //    new MoveAction(
-                        //        entity,
-                        //        new Point(path[0].X - entPos.X, path[0].Y - entPos.Y),
-                        //        CurrentMap,
-                        //        false
-                        //    )
-                        //);
-                        turn.Finish();
-                    }
-                    else
-                    {
-                        turn.Finish();
-                    }
-                }
-                else
-                {
-                    if (turn.HasMoves)
-                    {
-                        int x = Generator.NextDouble() < 0.5 ? 1 : 0;
-                        x *= Generator.NextDouble() < 0.5 ? -1 : 1;
-                        int y = Generator.NextDouble() < 0.5 ? 1 : 0;
-                        y *= Generator.NextDouble() < 0.5 ? -1 : 1;
-                        turn.AddAction(new MoveAction(entity, new Point(x, y), CurrentMap, false));
-                    }
-                    else
-                        break;
-                }
-            }
-            ActionStack.AddAction(turn);
+            ActionStack.AddAction(Processor.Process(entity, playerLoc, CurrentMap, Generator));
         }
     }
 }
