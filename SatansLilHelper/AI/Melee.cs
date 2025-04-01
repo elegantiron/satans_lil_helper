a6b96a9778f8f16f984d88247264261050bd2553
elegantiron <elegant.iron3167@fastmail.com>
2025-04-01 17:14:42 -0400

    Update Melee.cs

--- SatansLilHelper/AI/Melee.cs
+++ SatansLilHelper/AI/Melee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using Friflo.Engine.ECS;
 using Microsoft.Xna.Framework;
 using SatansLilHelper.Actions;
@@ -16,61 +17,62 @@ internal static partial class Processor
 {
     private static EntityTurn Melee(Entity entity, Location playerLoc, BaseMap map, IRandom rng)
     {
-        EntityTurn turn = new();
+        EntityTurn turn = new(entity);
         Location entLoc = entity.GetComponent<Location>();
         Point entPos = new(entLoc.X, entLoc.Y);
         int radius = Math.Min(
             EntityCalcs.GetStat(entity, AbilityID.Vision),
             EntityCalcs.GetStat(entity, AbilityID.LightRadius)
         );
-
-        List<Point> visibleTiles = ShadowCast.GetVisibleTiles(map, entPos, radius);
-
-        if (visibleTiles.Exists(test => test.X == playerLoc.X && test.Y == playerLoc.Y))
+        while (turn.HasActions)
         {
-            List<(int X, int Y)> path = Pathfinders.Dijkstra(entPos, playerLoc, map);
-            if (turn.HasSwift && false)
+            List<Point> visibleTiles = ShadowCast.GetVisibleTiles(map, entPos, radius);
+
+            if (visibleTiles.Exists(test => test.X == playerLoc.X && test.Y == playerLoc.Y))
             {
-                // Do swift action
-            }
-            else if (path.Count > 1 && turn.HasMoves)
-            {
-                turn.AddAction(
-                    new MoveAction(
-                        entity,
-                        new Point(path[0].X - entPos.X, path[0].Y - entPos.Y),
-                        map,
-                        false
-                    )
-                );
-            }
-            else if (path.Count == 1 && turn.HasAttacks)
-            {
-                turn.AddAction(
-                    new MeleeAction(
-                        entity,
-                        new Point(path[0].X - entPos.X, path[0].Y - entPos.Y),
-                        map,
-                        rng,
-                        false
-                    )
-                );
-                turn.Finish();
+                List<(int X, int Y)> path = Pathfinders.Dijkstra(entPos, playerLoc, map);
+                if (turn.HasSwift && false)
+                {
+                    // Do swift action
+                }
+                else if (path.Count > 1 && turn.HasMoves)
+                {
+                    turn.AddAction(
+                        new MoveAction(
+                            entity,
+                            new Point(path[0].X - entPos.X, path[0].Y - entPos.Y),
+                            map,
+                            false
+                        )
+                    );
+                }
+                else if (path.Count == 1 && turn.HasAttacks)
+                {
+                    turn.AddAction(
+                        new MeleeAction(
+                            entity,
+                            new Point(path[0].X - entPos.X, path[0].Y - entPos.Y),
+                            map,
+                            rng,
+                            false
+                        )
+                    );
+                }
+                else
+                {
+                    turn.Finish();
+                }
             }
             else
             {
-                turn.Finish();
-            }
-        }
-        else
-        {
-            if (turn.HasMoves)
-            {
-                int x = rng.NextDouble() < 0.5 ? 1 : 0;
-                x *= rng.NextDouble() < 0.5 ? -1 : 1;
-                int y = rng.NextDouble() < 0.5 ? 1 : 0;
-                y *= rng.NextDouble() < 0.5 ? -1 : 1;
-                turn.AddAction(new MoveAction(entity, new Point(x, y), map, false));
+                if (turn.HasMoves)
+                {
+                    int x = rng.NextDouble() < 0.5 ? 1 : 0;
+                    x *= rng.NextDouble() < 0.5 ? -1 : 1;
+                    int y = rng.NextDouble() < 0.5 ? 1 : 0;
+                    y *= rng.NextDouble() < 0.5 ? -1 : 1;
+                    turn.AddAction(new MoveAction(entity, new Point(x, y), map, false));
+                }
             }
         }
         return turn;
