61c6e24dcc9b9fdb142dc4d4a8aa5f7349df8c43
elegantiron <elegant.iron3167@fastmail.com>
2025-04-01 16:38:03 -0400

    Create AI.cs

---
+++ SatansLilHelper/AI/AI.cs
@@ -0,0 +1,28 @@
+﻿using Friflo.Engine.ECS;
+using SatansLilHelper.Components;
+using SatansLilHelper.Constants;
+using SatansLilHelper.Interfaces;
+using SatansLilHelper.Types;
+using SatansLilHelper.Utils.GameMaps;
+
+namespace SatansLilHelper.AI;
+
+internal static partial class AI
+{
+    public static EntityTurn Process(Entity entity, Location playerLoc, BaseMap map, IRandom rng)
+    {
+        EnemyType type = EnemyType.Wolf;
+        return type switch
+        {
+            EnemyType.Wolf => Melee(entity, playerLoc, map, rng),
+            _ => Default(entity),
+        };
+    }
+
+    private static EntityTurn Default(Entity entity)
+    {
+        EntityTurn turn = new(entity);
+        turn.Finish();
+        return turn;
+    }
+}
