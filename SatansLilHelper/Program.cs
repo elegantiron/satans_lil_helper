5569723327f5360f715ed72c8756e8b82e5088c2
elegantiron <elegant.iron3167@fastmail.com>
2025-04-06 15:18:41 -0400

    Update Program.cs

--- SatansLilHelper/Program.cs
+++ SatansLilHelper/Program.cs
@@ -10,7 +10,7 @@ try
 catch (Exception exception)
 {
     string FILE_PATH = AppContext.BaseDirectory + "error.txt";
-    using (StreamWriter writer = new StreamWriter(FILE_PATH, true))
+    using (StreamWriter writer = new StreamWriter(FILE_PATH, false))
     {
         writer.WriteLine("Date : " + DateTime.Now.ToString());
         writer.WriteLine();
