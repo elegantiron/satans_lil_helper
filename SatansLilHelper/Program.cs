9c8a1c538f7f0a2a962c1f7c74bd1b58fa8aa956
elegantiron <elegant.iron3167@fastmail.com>
2025-04-06 14:58:13 -0400

    Update Program.cs

--- SatansLilHelper/Program.cs
+++ SatansLilHelper/Program.cs
@@ -1,2 +1,27 @@
-﻿using var game = new SatansLilHelper.Engine();
-game.Run();
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+try
+{
+    using var game = new SatansLilHelper.Engine();
+    game.Run();
+}
+catch (Exception exception)
+{
+    string FILE_PATH = AppContext.BaseDirectory + "error.txt";
+    using (StreamWriter writer = new StreamWriter(FILE_PATH, true))
+    {
+        writer.WriteLine("Date : " + DateTime.Now.ToString());
+        writer.WriteLine();
+
+        while (exception != null)
+        {
+            writer.WriteLine(exception.GetType().FullName);
+            writer.WriteLine("Message : " + exception.Message);
+            writer.WriteLine(" StackTrace : " + exception.StackTrace);
+
+            exception = exception.InnerException;
+        }
+    }
+}
