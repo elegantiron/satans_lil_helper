50e59dc5a135e89cc7ab3397ece7f2413e3d46e2
elegantiron <elegant.iron3167@fastmail.com>
2025-04-07 17:32:28 -0400

    Update SpellBookInputHandler.cs

--- SatansLilHelper/InputHandlers/SpellBookInputHandler.cs
+++ SatansLilHelper/InputHandlers/SpellBookInputHandler.cs
@@ -1,21 +1,23 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using Friflo.Engine.ECS;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
+using SatansLilHelper.Components;
 using SatansLilHelper.Constants;
+using SatansLilHelper.Extensions;
 using SatansLilHelper.Interfaces;
 using SatansLilHelper.Properties;
-using SatansLilHelper.Types;
 
 namespace SatansLilHelper.InputHandlers;
 
-internal class SpellBookInputHandler(IInputHandler parent) : IInputHandler
+internal class SpellBookInputHandler(GameInputHandler parent) : IInputHandler
 {
-    private IInputHandler _parent = parent;
-    private VecPair _titleVecs = new();
-    private Rectangle _shadeShape = Rectangle.Empty;
+    private GameInputHandler _parent = parent;
+    private int _index = 0;
 
     public void Draw(
         SpriteBatch spriteBatch,
@@ -25,43 +27,76 @@ internal class SpellBookInputHandler(IInputHandler parent) : IInputHandler
         Dictionary<FontID, SpriteFont> fontMap
     )
     {
-        if (_titleVecs.Location == Vector2.Zero)
-        {
-            CalculateVectors(spriteBatch, fontMap);
-        }
         _parent.Draw(spriteBatch, textureMap, effectMap, songMap, fontMap);
-        spriteBatch.Draw(textureMap[TextureID.WhitePixel], _shadeShape, Colors.TranslucentBlack);
+        Viewport viewport = spriteBatch.GraphicsDevice.Viewport;
+        Rectangle shadeShape = new(
+            viewport.Width / 10,
+            viewport.Height / 10,
+            viewport.Width * 8 / 10,
+            viewport.Height * 8 / 10
+        );
+        spriteBatch.Draw(textureMap[TextureID.WhitePixel], shadeShape, Colors.TranslucentBlack);
+        Vector2 size = fontMap[FontID.Menu].MeasureString(GameStrings.SpellBookTitle);
         spriteBatch.DrawString(
             fontMap[FontID.Menu],
-            GameStrings.SpellBook,
-            _titleVecs.Location,
+            GameStrings.SpellBookTitle,
+            new Vector2(
+                spriteBatch.GraphicsDevice.Viewport.Width / 2.0f,
+                (float)shadeShape.Y + size.Y
+            ),
             Colors.White,
-            0f,
-            _titleVecs.Origin,
-            1f,
-            SpriteEffects.None,
-            0f
+            new Vector2(size.X / 2, size.Y / 2)
         );
+        Vector2 textDest = new(shadeShape.X + 25, shadeShape.Y + 2.5f * size.Y);
+        EntityLinks<Grimoire> entities = _parent.CurrentMap.Player.GetIncomingLinks<Grimoire>();
+        if (_index < 0)
+            _index = entities.Count - 1;
+        else if (_index >= entities.Count)
+            _index %= entities.Count;
+        if (entities.Count > 0)
+        {
+            foreach (int idx in Enumerable.Range(0, entities.Count))
+            {
+                if (textDest.Y > shadeShape.Height)
+                {
+                    textDest.Y = shadeShape.Y + 2.5f * size.Y;
+                    textDest.X += (shadeShape.Width - 50) / 3;
+                    if (textDest.X > shadeShape.Width)
+                        break;
+                }
+
+                EntityName entName = entities.Entities[idx].GetComponent<EntityName>();
+                string displayName = "" + entName.value;
+
+                spriteBatch.DrawString(fontMap[FontID.Status], displayName, textDest, Colors.White);
+                if (idx == _index)
+                {
+                    spriteBatch.Draw(
+                        textureMap[TextureID.ArrowBlue],
+                        new Vector2(textDest.X - 30, textDest.Y + 3),
+                        Colors.White
+                    );
+                }
+                textDest.Y += fontMap[FontID.Status].LineSpacing * 1.15f;
+            }
+        }
     }
 
     public IInputHandler HandleKey(Keys key)
     {
-        return key switch
+        switch (key)
         {
-            Keys.S => _parent,
-            Keys.Escape => _parent,
-            _ => this,
-        };
-    }
-
-    private void CalculateVectors(SpriteBatch spriteBatch, Dictionary<FontID, SpriteFont> fontMap)
-    {
-        _titleVecs.Location.X = spriteBatch.GraphicsDevice.Viewport.Width / 2;
-        _titleVecs.Origin.X = fontMap[FontID.Menu].MeasureString(GameStrings.SpellBook).X / 2;
-        _shadeShape.X = spriteBatch.GraphicsDevice.Viewport.Width / 8;
-        _shadeShape.Y = spriteBatch.GraphicsDevice.Viewport.Height / 8;
-        _shadeShape.Width = spriteBatch.GraphicsDevice.Viewport.Width * 6 / 8;
-        _shadeShape.Height = spriteBatch.GraphicsDevice.Viewport.Height * 6 / 8;
-        _titleVecs.Location.Y = _shadeShape.Y + 10;
+            case Keys.S:
+                return _parent;
+            case Keys.Escape:
+                return _parent;
+            case Keys.Up:
+                _index--;
+                break;
+            case Keys.Down:
+                _index++;
+                break;
+        }
+        return this;
     }
 }
