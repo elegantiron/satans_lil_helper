79bc13b065b7932f498cb0a35eb79f101692b646
elegantiron <elegant.iron3167@fastmail.com>
2025-04-08 16:52:55 -0400

    rename class

---
+++ SatansLilHelper/Components/Effect.cs
@@ -0,0 +1,11 @@
+﻿using Friflo.Engine.ECS;
+using SatansLilHelper.Constants;
+
+namespace SatansLilHelper.Components;
+
+[ComponentKey("effect-power")]
+internal struct Effect(ItemEffect effect, int value) : IComponent
+{
+    public int Value = value;
+    public ItemEffect Type = effect;
+}
