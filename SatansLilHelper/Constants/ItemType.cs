b8e058539b0a43f67ba6b2ff59d20fb4e076a361
elegantiron <128913032+elegantiron@users.noreply.github.com>
2025-04-08 13:25:04 -0400

    add torch type

--- SatansLilHelper/Constants/ItemType.cs
+++ SatansLilHelper/Constants/ItemType.cs
@@ -9,8 +9,8 @@ namespace SatansLilHelper.Constants;
 public enum ItemType
 {
     // Base item types
-    None = 0 << 0,
-    Weapon1H = 1 << 0,
+    None = 0,
+    Weapon1H = 1,
     Shield = 1 << 1,
     Armor = 1 << 2,
     Helm = 1 << 3,
@@ -19,8 +19,9 @@ public enum ItemType
     RingL = 1 << 6,
     RingR = 1 << 7,
     Amulet = 1 << 8,
+    Torch = 1 << 9,
 
     // Common combinations
-    Base = RingL | RingR | Amulet,
+    Base = RingL | RingR | Amulet | Torch,
     Warrior = Base | Weapon1H | Shield | Armor | Helm | Gauntlets | Boots,
 }
