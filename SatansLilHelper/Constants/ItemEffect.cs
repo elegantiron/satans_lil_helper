6be58cdb2ee8ef69f0b87770a63f8ecc240116a8
elegantiron <elegant.iron3167@fastmail.com>
2025-04-08 16:53:00 -0400

    Create ItemEffect.cs

---
+++ SatansLilHelper/Constants/ItemEffect.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SatansLilHelper.Constants;
+
+internal enum ItemEffect
+{
+    None,
+    Heal,
+    Harm,
+    Cloud,
+}
