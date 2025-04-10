16c6d2bf9de67f22d25dbcab7054a34a92bc2105
elegantiron <elegant.iron3167@fastmail.com>
2025-04-10 17:55:41 -0400

    Update IDrawable.cs

--- SatansLilHelper/Interfaces/IDrawable.cs
+++ SatansLilHelper/Interfaces/IDrawable.cs
@@ -1,9 +1,8 @@
 ﻿using System.Collections.Generic;
-
+using Apos.Camera;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Media;
-
 using SatansLilHelper.Constants;
 
 namespace SatansLilHelper.Interfaces;
@@ -12,6 +11,7 @@ internal interface IDrawable
 {
     void Draw(
         SpriteBatch spriteBatch,
+        Camera camera,
         Dictionary<TextureID, Texture2D> textureMap,
         Dictionary<EffectID, SoundEffect> effectMap,
         Dictionary<SongID, Song> songMap,
