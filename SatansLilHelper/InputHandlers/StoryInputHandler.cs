d4b480d16d7d2fb3b3c0511e6b7bff674b7486bf
elegantiron <elegant.iron3167@fastmail.com>
2025-04-10 17:55:36 -0400

    Update StoryInputHandler.cs

--- SatansLilHelper/InputHandlers/StoryInputHandler.cs
+++ SatansLilHelper/InputHandlers/StoryInputHandler.cs
@@ -1,10 +1,9 @@
 ﻿using System.Collections.Generic;
-
+using Apos.Camera;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
-
 using SatansLilHelper.Constants;
 using SatansLilHelper.Interfaces;
 
@@ -21,13 +20,14 @@ internal class StoryInputHandler(IInputHandler parent, FontID font, JournalID en
 
     public void Draw(
         SpriteBatch spriteBatch,
+        Camera camera,
         Dictionary<TextureID, Texture2D> textureMap,
         Dictionary<EffectID, SoundEffect> effectMap,
         Dictionary<SongID, Song> songMap,
         Dictionary<FontID, SpriteFont> fontMap
     )
     {
-        _parent.Draw(spriteBatch, textureMap, effectMap, songMap, fontMap);
+        _parent.Draw(spriteBatch, camera, textureMap, effectMap, songMap, fontMap);
     }
 
     public IInputHandler HandleKey(Keys key)
