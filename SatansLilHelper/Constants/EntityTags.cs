f019d2c23b7e3ecd998c94ff642f4f79a59f7105
elegantiron <elegant.iron3167@fastmail.com>
2025-04-11 15:52:39 -0400

    Update EntityTags.cs

--- SatansLilHelper/Constants/EntityTags.cs
+++ SatansLilHelper/Constants/EntityTags.cs
@@ -2,24 +2,34 @@
 
 namespace SatansLilHelper.Constants;
 
+internal struct Activatable : ITag { }
+
 internal struct Actor : ITag { }
 
 internal struct Alive : ITag { }
 
 internal struct Blocking : ITag { }
 
+internal struct DamagesInterruptor : ITag { }
+
+internal struct Equippable : ITag { }
+
 internal struct Hostile : ITag { }
+
+internal struct Interruptible : ITag { }
 
 internal struct Invisible : ITag { }
 
 internal struct Item : ITag { }
+
+internal struct MovesActor : ITag { }
+
+internal struct MovesTarget : ITag { }
+
+internal struct Passive : ITag { }
 
 internal struct Player : ITag { }
 
 internal struct Skill : ITag { }
 
 internal struct Visible : ITag { }
-
-internal struct Equippable : ITag { }
-
-internal struct Activatable : ITag { }
