548647357fcbff0110b66ddc738adbe9b53f7e66
elegantiron <elegant.iron3167@fastmail.com>
2025-04-11 19:50:25 -0400

    Create RandomEffect.cs

---
+++ SatansLilHelper/Components/RandomEffect.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Friflo.Engine.ECS;
+using SatansLilHelper.Constants;
+
+namespace SatansLilHelper.Components;
+
+internal struct RandomEffect : IComponent
+{
+    public ItemEffect Effect;
+    public int Dice,
+        Sides,
+        Bonus;
+
+    public RandomEffect(ItemEffect effect, int dice, int sides, int bonus = 0)
+    {
+        Effect = effect;
+        Dice = dice;
+        Sides = sides;
+        Bonus = bonus;
+    }
+}
