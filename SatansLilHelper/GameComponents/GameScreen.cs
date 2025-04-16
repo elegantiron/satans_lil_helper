2e7ca8c967e7b6e1d5b34b1977892038a2be1b86
elegantiron <128913032+elegantiron@users.noreply.github.com>
2025-04-16 14:11:23 -0400

    Update GameScreen.cs

--- SatansLilHelper/GameComponents/GameScreen.cs
+++ SatansLilHelper/GameComponents/GameScreen.cs
@@ -4,6 +4,7 @@ using Friflo.Engine.ECS;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MLEM.Input;
 using SatansLilHelper.Constants;
 using SatansLilHelper.ECSComponents;
 using SatansLilHelper.Extensions;
@@ -15,11 +16,35 @@ internal class GameScreen : DrawableGameComponent
     private SpriteBatch? _spriteBatch;
     private Camera? _camera;
     private Dictionary<TextureID, Texture2D> _textures;
+    private Keybind movement,
+        up,
+        upRight,
+        right,
+        downRight,
+        down,
+        downLeft,
+        left,
+        upLeft;
 
     public GameScreen(Game game)
         : base(game)
     {
         _textures = [];
+
+        up = new Keybind().Add(Keys.Up).Add(Keys.NumPad8);
+        upRight = new Keybind().Add(Keys.NumPad9);
+        right = new Keybind().Add(Keys.Right).Add(Keys.NumPad6);
+        downRight = new Keybind().Add(Keys.NumPad3);
+        down = new Keybind().Add(Keys.Down).Add(Keys.NumPad2);
+        downLeft = new Keybind().Add(Keys.NumPad1);
+        left = new Keybind().Add(Keys.Left).Add(Keys.NumPad4);
+        upLeft = new Keybind().Add(Keys.NumPad7);
+
+        List<Keybind> keybinds = [up, upRight, right, downRight, down, downLeft, left, upLeft];
+        movement = new();
+        foreach (var bind in keybinds)
+        foreach (var combo in bind.Combinations)
+            movement.Add(combo);
     }
 
     public override void Update(GameTime gameTime)
@@ -30,7 +55,8 @@ internal class GameScreen : DrawableGameComponent
         Location playerLoc = engine.World.CurrentMap.Player.GetComponent<Location>();
         _camera.XY = new Vector2(playerLoc.X * 32, playerLoc.Y * 32);
         engine.World.CurrentMap.UpdatePlayerVision();
-        if (engine.Handler.TryConsumePressed(Keys.Escape))
+        if (movement.IsPressedAvailable(engine.Handler)) { }
+        else if (engine.Handler.TryConsumePressed(Keys.Escape))
         {
             Enabled = false;
             engine.PauseMenu.Enabled = true;
@@ -41,6 +67,7 @@ internal class GameScreen : DrawableGameComponent
         else if (engine.Handler.TryConsumePressed(Keys.M))
         {
             Enabled = Visible = engine.StatusScreen.Enabled = engine.StatusScreen.Visible = false;
+            engine.MegaMap.Enabled = engine.MegaMap.Visible = true;
         }
         else if (engine.Handler.TryConsumePressed(Keys.S))
         {
