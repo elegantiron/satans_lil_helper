479955205f26eac4b8d2729ade44ce3e46784d74
elegantiron <128913032+elegantiron@users.noreply.github.com>
2025-04-17 14:28:21 -0400

    Create IncomingLinks.cs

---
+++ SatansLilHelper/GameComponents/IncomingLinks.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Friflo.Engine.ECS;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SatansLilHelper.GameComponents;
+
+internal class IncomingLinks : DrawableGameComponent
+{
+    private Type? _type;
+    private SpriteBatch? batch;
+
+    public IncomingLinks(Game game)
+        : base(game) { }
+
+    public override void Initialize()
+    {
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        batch = new(Game.GraphicsDevice);
+    }
+
+    public void SetType(Type type)
+    {
+        if (type is not ILinkComponent)
+            throw new ArgumentException("Type must be an ILinkComponent", nameof(type));
+        _type = type;
+    }
+}
