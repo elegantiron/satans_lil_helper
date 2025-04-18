f277edbde2ef4946d088c69d256e69979b7b09e2
elegantiron <128913032+elegantiron@users.noreply.github.com>
2025-04-18 13:18:14 -0400

    Update IncomingLinks.cs

--- SatansLilHelper/GameComponents/IncomingLinks.cs
+++ SatansLilHelper/GameComponents/IncomingLinks.cs
@@ -7,13 +7,23 @@ using System.Threading.Tasks;
 using Friflo.Engine.ECS;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MLEM.Font;
+using MLEM.Textures;
+using MLEM.Ui;
+using MLEM.Ui.Elements;
+using MLEM.Ui.Style;
+using SatansLilHelper.Constants;
 
 namespace SatansLilHelper.GameComponents;
 
-internal class IncomingLinks : DrawableGameComponent
+internal class IncomingLinks<TComponent> : DrawableGameComponent
+    where TComponent : struct, ILinkComponent
 {
-    private Type? _type;
     private SpriteBatch? batch;
+    private UiSystem? system;
+    private UiStyle? style;
+    private Panel? panel;
 
     public IncomingLinks(Game game)
         : base(game) { }
@@ -23,15 +33,69 @@ internal class IncomingLinks : DrawableGameComponent
         base.Initialize();
     }
 
+    protected override void OnEnabledChanged(object sender, EventArgs args)
+    {
+        base.OnEnabledChanged(sender, args);
+        if (Game is not Engine engine)
+            return;
+        if (Enabled)
+        {
+            panel?.RemoveChildren((Element _) => true);
+            EntityLinks<TComponent> entityLinks =
+                engine.World.Player.GetIncomingLinks<TComponent>();
+        }
+    }
+
     protected override void LoadContent()
     {
         batch = new(Game.GraphicsDevice);
+        style = new UntexturedStyle(batch)
+        {
+            Font = new GenericSpriteFont(Game.Content.Load<SpriteFont>(FilePaths.MenuFont)),
+            TextColor = Colors.Black,
+            TooltipTextWidth = 250,
+            TextAlignment = MLEM.Formatting.TextAlignment.Center,
+            PanelTexture = new NinePatch(
+                new TextureRegion(
+                    Game.Content.Load<Texture2D>(@"Images/UI/Panels/panel_brown_damaged_dark")
+                ),
+                8f,
+                NinePatchMode.Tile
+            ),
+            TooltipDelay = new TimeSpan(0, 0, 0, 0, 350),
+            ButtonTexture = new NinePatch(
+                new TextureRegion(
+                    Game.Content.Load<Texture2D>(@"Images/UI/Panels/panel_brown_damaged")
+                ),
+                8f,
+                NinePatchMode.Tile
+            ),
+            SelectionIndicator = new NinePatch(
+                new TextureRegion(
+                    Game.Content.Load<Texture2D>(@"Images/UI/Panels/panel_border_grey_detail")
+                ),
+                19f,
+                NinePatchMode.Tile
+            ),
+        };
+        system = new(Game, style);
+        panel = new(Anchor.Center, new Vector2(0.5f));
     }
 
-    public void SetType(Type type)
+    public override void Draw(GameTime gameTime) { }
+
+    public override void Update(GameTime gameTime)
     {
-        if (type is not ILinkComponent)
-            throw new ArgumentException("Type must be an ILinkComponent", nameof(type));
-        _type = type;
+        if (Game is not Engine engine || system is null)
+            return;
+        base.Update(gameTime);
+        if (engine.Handler.TryConsumePressed(Keys.Space))
+            OnEnabledChanged(new object(), new EventArgs());
+    }
+
+    private Button MakeButton(Anchor anchor, Vector2 size, string text)
+    {
+        Button newButton = new(anchor, size, text);
+        throw new NotImplementedException();
     }
 }
