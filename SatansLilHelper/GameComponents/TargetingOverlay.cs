ea7f60c5630796fda8b86af9f92777995dd6d9e4
elegantiron <elegant.iron3167@fastmail.com>
2025-04-27 13:42:54 -0400

    Update TargetingOverlay.cs

--- SatansLilHelper/GameComponents/TargetingOverlay.cs
+++ SatansLilHelper/GameComponents/TargetingOverlay.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Apos.Camera;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using SatansLilHelper.Utils;
 
 namespace SatansLilHelper.GameComponents;
 
@@ -34,5 +35,46 @@ internal class TargetingOverlay : DrawableGameComponent
         batch.Begin();
 
         batch.End();
+    }
+
+    public override void Update(GameTime gameTime)
+    {
+        if (Game is not Engine engine)
+            return;
+
+        if (Settings.Default.Movement.IsPressedAvailable(engine.Handler))
+            ProcessMovement();
+    }
+
+    private void ProcessMovement()
+    {
+        if (Game is not Engine engine)
+            return;
+        int dx = 0,
+            dy = 0;
+        if (
+            Settings.Default.MoveUp.TryConsumePressed(engine.Handler)
+            || Settings.Default.MoveUpLeft.IsPressedAvailable(engine.Handler)
+            || Settings.Default.MoveUpRight.IsPressedAvailable(engine.Handler)
+        )
+            dy = -1;
+        if (
+            Settings.Default.MoveUpRight.TryConsumePressed(engine.Handler)
+            || Settings.Default.MoveRight.TryConsumePressed(engine.Handler)
+            || Settings.Default.MoveDownRight.IsPressedAvailable(engine.Handler)
+        )
+            dx = 1;
+        if (
+            Settings.Default.MoveDownRight.TryConsumePressed(engine.Handler)
+            || Settings.Default.MoveDown.TryConsumePressed(engine.Handler)
+            || Settings.Default.MoveDownLeft.IsPressedAvailable(engine.Handler)
+        )
+            dy = 1;
+        if (
+            Settings.Default.MoveDownLeft.TryConsumePressed(engine.Handler)
+            || Settings.Default.MoveLeft.TryConsumePressed(engine.Handler)
+            || Settings.Default.MoveUpLeft.TryConsumePressed(engine.Handler)
+        )
+            dx = -1;
     }
 }
