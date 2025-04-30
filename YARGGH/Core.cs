1db0a9070f708b82e8b41d54e99c2ce7dcb30fd5
elegantiron <elegant.iron3167@fastmail.com>
2025-04-30 18:59:33 -0400

    add audio controller

--- YARGGH/Core.cs
+++ YARGGH/Core.cs
@@ -4,6 +4,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using YARGGH.Audio;
 using YARGGH.Input;
 
 namespace YARGGH;
@@ -46,6 +47,11 @@ public class Core : Game
     /// Gets or Sets a value that indicates if the game should exit when the esc key on the keyboard is pressed.
     /// </summary>
     public static bool ExitOnEscape { get; set; }
+
+    /// <summary>
+    /// Gets a reference to the audio control system.
+    /// </summary>
+    public static AudioController Audio { get; private set; }
 
     /// <summary>
     /// Get this game's storage directory
@@ -104,11 +110,26 @@ public class Core : Game
 
         // Create a new input manager
         Input = new();
+
+        // Create a new audio controller.
+        Audio = new AudioController();
+    }
+
+    protected override void UnloadContent()
+    {
+        // Dispose of the audio controller
+        Audio.Dispose();
+
+        base.UnloadContent();
     }
 
     protected override void Update(GameTime gameTime)
     {
+        // Update the input manager.
         Input.Update(gameTime);
+
+        // Update the audio controller.
+        Audio.Update();
 
         if (ExitOnEscape && Input.Keyboard.IsKeyDown(Keys.Escape))
             Exit();
