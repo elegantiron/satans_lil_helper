c9030d0d67c11319b0bc169eced8ddd06e3941f9
elegantiron <elegant.iron3167@fastmail.com>
2025-05-02 17:39:33 -0400

    Create Tile.cs

---
+++ YARGGH/MapGen/Tile.cs
@@ -0,0 +1,11 @@
+﻿namespace YARGGH.MapGen;
+
+public struct Tile(bool walkable, bool passesLight)
+{
+    public bool Walkable = walkable;
+    public bool PassesLight = passesLight;
+    public bool Visible = false;
+    public bool Explored = false;
+    public float LightDistance = 0f;
+    public int MovementCost = int.MinValue;
+}
