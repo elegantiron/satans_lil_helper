02dfd7390195b9bcee18a696f3ba4686df3cd23b
elegantiron <elegant.iron3167@fastmail.com>
2025-10-02 16:36:05 -0400

    set up assets

---
+++ assets/SatanAssets.c
@@ -0,0 +1,31 @@
+#include "SatanAssets.h"
+
+const unsigned char fairy_dust[] = {
+#embed "Fonts/FairyDustB.ttf"
+};
+
+Asset FairyDust = {fairy_dust, sizeof(fairy_dust)};
+
+const unsigned char crayon_libre[] = {
+#embed "Fonts/CrayonLibre.ttf"
+};
+
+Asset CrayonLibre = {crayon_libre, sizeof(crayon_libre)};
+
+const unsigned char hide_and_seek[] = {
+// #embed "Music/hide_and_seek.wav"
+};
+
+Asset HideAndSeek = {hide_and_seek, sizeof(hide_and_seek)};
+
+const unsigned char night_time_scavenge1[] = {
+// #embed "Music/night_time_scavenge1.wav"
+};
+
+Asset NightTimeScavenge1 = {night_time_scavenge1, sizeof(night_time_scavenge1)};
+
+const unsigned char night_time_scavenge2[] = {
+// #embed "Music/night_time_scavenge2.wav"
+};
+
+Asset NightTimeScavenge2 = {night_time_scavenge2, sizeof(night_time_scavenge2)};
---
+++ assets/SatanAssets.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <stddef.h>
+typedef struct {
+    const unsigned char* data;
+    size_t               size;
+} Asset;
