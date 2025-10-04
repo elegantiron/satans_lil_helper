4ffd1d543ed08be397148e07fffb1c5968813b6e
elegantiron <elegant.iron3167@fastmail.com>
2025-10-04 12:10:39 -0400

    Create Engine.hpp

---
+++ lib/inc/libsatan/Engine.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "libsatan/Engine/Game.hpp"       // IWYU pragma: export
+#include "libsatan/Engine/Scene.hpp"      // IWYU pragma: export
+#include "libsatan/Engine/SceneStack.hpp" // IWYU pragma: export
