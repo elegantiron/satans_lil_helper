9564c31a0d460f8d7c9030aa0ff2c800a6a50cc2
elegantiron <elegant.iron3167@fastmail.com>
2025-10-04 12:50:30 -0400

    fix file name

--- lib/inc/libsatan/Engine.hpp
+++ lib/inc/libsatan/Engine.hpp
@@ -2,4 +2,4 @@
 
 #include "libsatan/Engine/Game.hpp"       // IWYU pragma: export
 #include "libsatan/Engine/Scene.hpp"      // IWYU pragma: export
-#include "libsatan/Engine/SceneStack.hpp" // IWYU pragma: export
+#include "libsatan/Engine/SceneManager.hpp" // IWYU pragma: export
