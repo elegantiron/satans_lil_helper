1da1e376d31ec9a6877629cb109d2a5d6b82ee02
elegantiron <elegant.iron3167@fastmail.com>
2025-10-04 10:27:47 -0400

    Create Constants.hpp

---
+++ game/inc/Constants.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include "GameString.hpp"
+
+#include <SFML/System/Vector2.hpp>
+
+namespace SatansLilHelper::Constants {
+    inline constexpr GameString   Title      = {"Satan's Lil Helper", 85};
+    inline constexpr GameString   PressStart = {"Press START", 45};
+    inline constexpr sf::Vector2u WindowSize = {1280, 720};
+
+}
