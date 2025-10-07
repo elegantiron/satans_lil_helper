87e5869afbfca673e20b0dc42fedd7311e71a890
elegantiron <elegant.iron3167@fastmail.com>
2025-10-07 10:46:11 -0400

    add assets

--- assets/SatanAssets.c
+++ assets/SatanAssets.c
@@ -22,10 +22,30 @@ const unsigned char night_time_scavenge1[] = {
 #embed "Music/night_time_scavenge1.wav"
 };
 
-const Asset NightTimeScavenge1 = {night_time_scavenge1, sizeof(night_time_scavenge1)};
+const Asset NightTimeScavenge1
+    = {night_time_scavenge1, sizeof(night_time_scavenge1)};
 
 const unsigned char night_time_scavenge2[] = {
 #embed "Music/night_time_scavenge2.wav"
 };
 
-const Asset NightTimeScavenge2 = {night_time_scavenge2, sizeof(night_time_scavenge2)};
+const Asset NightTimeScavenge2
+    = {night_time_scavenge2, sizeof(night_time_scavenge2)};
+
+const unsigned char satan_main[] = {
+#embed "Images/Satan/main.png"
+};
+
+const Asset SatanMain = {satan_main, sizeof(satan_main)};
+
+const unsigned char satan_eyes_open[] = {
+#embed "Images/Satan/eyes_open.png"
+};
+
+const Asset SatanEyesOpen = {satan_eyes_open, sizeof(satan_eyes_open)};
+
+const unsigned char satan_mouth_closed[] = {
+#embed "Images/Satan/mouth_closed.png"
+};
+
+const Asset SatanMouthClosed = {satan_mouth_closed, sizeof(satan_mouth_closed)};
