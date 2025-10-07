e54707a2f050c257f892bdc0b0c72484e3343837
elegantiron <elegant.iron3167@fastmail.com>
2025-10-07 10:53:43 -0400

    add text and sprites

--- game/inc/Scenes/Title.hpp
+++ game/inc/Scenes/Title.hpp
@@ -5,18 +5,41 @@
 #include <SFML/Graphics/Font.hpp>
 #include <SFML/Graphics/Text.hpp>
 
+extern const Asset FairyDust;
+extern const Asset CrayonLibre;
+extern const Asset SatanMain;
+extern const Asset SatanEyesOpen;
+extern const Asset SatanMouthClosed;
+
 namespace SatansLilHelper::Scenes {
     class Title : public Engine::Scene {
-        sf::Font _font;
-        sf::Text _title;
-        sf::Text _pressStart;
+        sf::Font    _titleFont{FairyDust.data, FairyDust.size};
+        sf::Text    _title{_titleFont};
+        sf::Font    _startFont{CrayonLibre.data, CrayonLibre.size};
+        sf::Text    _pressStart{_startFont};
+        sf::Texture _satanMainText{SatanMain.data, SatanMain.size};
+        sf::Sprite  _satanMain{_satanMainText};
+        sf::Texture _satanEyesOpenText{SatanEyesOpen.data, SatanEyesOpen.size};
+        sf::Sprite  _satanEyesOpen{_satanEyesOpenText};
+        sf::Texture _satanMouthClosedText{SatanMouthClosed.data,
+                                          SatanMouthClosed.size};
+        sf::Sprite  _satanMouthClosed{_satanMouthClosedText};
 
+        void configureTexts();
+        void configureSprites();
+        void setSpritePositions();
         void setTextPositions();
         void init() override;
         void draw(sf::RenderTarget& target,
                   sf::RenderStates  states) const override;
+        void iterate(const GameTime& gameTime,
+                     bool&           successful,
+                     bool&           keepRunning) override;
+        void handleEvent(std::optional<sf::Event> event,
+                         bool&                    successful,
+                         bool&                    keepRunning) override;
 
     public:
-        Title();
+        Title() = default;
     };
 }
--- game/src/Scenes/Title.cpp
+++ game/src/Scenes/Title.cpp
@@ -1,7 +1,5 @@
 #include "Scenes/Title.hpp"
 
-extern const Asset FairyDust;
-
 namespace { // Local constants
     constexpr unsigned int TITLE_CHAR_SIZE      = 85;
     constexpr int          TITLE_Y_POS          = 15;
@@ -9,12 +7,20 @@ namespace { // Local constants
     constexpr sf::Color    TITLE_OUTLINE_COLOR  = {0xFF, 0x00, 0x00, 0xFF};
     constexpr unsigned int PSTART_CHAR_SIZE     = 35;
     constexpr float        PSTART_Y_POS_FACTOR  = 4.F / 5.F;
-    constexpr sf::Color    PSTART_FILL_COLOR    = {0xF0, 0xF0, 0xF0, 0xFF};
-    constexpr sf::Color    PSTART_OUTLINE_COLOR = {0xFF, 0xFF, 0xFF, 0xFF};
+    constexpr sf::Color    PSTART_FILL_COLOR    = {0x60, 0x60, 0x60, 0xFF};
+    constexpr sf::Color    PSTART_OUTLINE_COLOR = {0x60, 0x00, 0x00, 0xFF};
 } // namespace
 
 namespace SatansLilHelper::Scenes {
     void Title::init() {
+        configureTexts();
+        configureSprites();
+
+        setTextPositions();
+        setSpritePositions();
+    }
+
+    void Title::configureTexts() {
         _title.setString(Constants::Title);
         _title.setCharacterSize(TITLE_CHAR_SIZE);
         auto bounds = _title.getLocalBounds();
@@ -28,13 +34,27 @@ namespace SatansLilHelper::Scenes {
         _pressStart.setOrigin(sf::Vector2f(bounds.size.x / 2, 0));
         _pressStart.setFillColor(PSTART_FILL_COLOR);
         _pressStart.setOutlineColor(PSTART_OUTLINE_COLOR);
-
-        setTextPositions();
+        _pressStart.setOutlineThickness(1);
     }
 
-    Title::Title()
-        : _font(FairyDust.data, FairyDust.size), _title(_font),
-          _pressStart(_font) {}
+    void Title::configureSprites() {
+        auto bounds = _satanMain.getLocalBounds();
+        _satanMain.setOrigin({bounds.size.x / 2, bounds.size.y / 2});
+        bounds = _satanEyesOpen.getLocalBounds();
+        _satanEyesOpen.setOrigin({bounds.size.x / 2, bounds.size.y / 2});
+        bounds = _satanMouthClosed.getLocalBounds();
+        _satanMouthClosed.setOrigin({bounds.size.x / 2, bounds.size.y / 2});
+    }
+
+    void Title::setSpritePositions() {
+        auto&       core   = Engine::Core::getInstance();
+        const auto& window = core.getWindow();
+        auto        bounds = window.getSize();
+        _satanMain.setPosition({(float)bounds.x / 2, (float)bounds.y / 2});
+        _satanEyesOpen.setPosition({(float)bounds.x / 2, (float)bounds.y / 2});
+        _satanMouthClosed.setPosition(
+            {(float)bounds.x / 2, (float)bounds.y / 2});
+    }
 
     void Title::setTextPositions() {
         auto&       core       = Engine::Core::getInstance();
@@ -49,5 +69,29 @@ namespace SatansLilHelper::Scenes {
     void Title::draw(sf::RenderTarget& target, sf::RenderStates states) const {
         target.draw(_title, states);
         target.draw(_pressStart, states);
+        target.draw(_satanMain, states);
+        target.draw(_satanEyesOpen, states);
+        target.draw(_satanMouthClosed, states);
     }
+
+    void Title::iterate(const GameTime& /*gameTime*/,
+                        bool& successful,
+                        bool& keepRunning) {
+        successful = keepRunning = true;
+    }
+
+    void Title::handleEvent(std::optional<sf::Event> event,
+                            bool&                    successful,
+                            bool&                    keepRunning) {
+        successful = keepRunning = true;
+        if (event->is<sf::Event::Closed>()) {
+            keepRunning = false;
+            return;
+        }
+        if (event->is<sf::Event::Resized>()) {
+            // setSpritePositions();
+            // setTextPositions();
+        }
+    }
+
 } // namespace SatansLilHelper::Scenes
