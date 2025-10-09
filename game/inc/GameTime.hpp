56b816aca0d9fe4fe33fe2dab095cf393b47b66d
elegantiron <128913032+elegantiron@users.noreply.github.com>
2025-10-09 13:24:20 -0400

    move using declarations to separate file

--- game/inc/GameTime.hpp
+++ game/inc/GameTime.hpp
@@ -1,12 +1,6 @@
 #pragma once
-#include <chrono>
 
-namespace {
-    inline constexpr int MILLIS_PER_SECOND = 1000;
-}
 
-using Duration
-    = std::chrono::duration<long double, std::ratio<1, MILLIS_PER_SECOND>>;
 
 struct GameTime {
     Duration totalElapsedTime{0};
---
+++ game/inc/Usings.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include <chrono>
+
+namespace {
+    inline constexpr int MILLIS_PER_SECOND = 1000;
+}
+
+using Duration
+    = std::chrono::duration<long double, std::ratio<1, MILLIS_PER_SECOND>>;
+
+using TimerCallback = void (*)(bool& runAgain, Duration& nextInterval);
