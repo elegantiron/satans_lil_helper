dedc2251430645b47ebc11e2481a8a7bb06db34c
elegantiron <elegant.iron3167@fastmail.com>
2025-10-09 16:48:17 -0400

    fix timer templating

--- game/inc/Timer.hpp
+++ game/inc/Timer.hpp
@@ -11,16 +11,36 @@ namespace SatansLilHelper {
 
     public:
         // Timer(TimerCallback<T> callback, Duration interval);
-        void update(const GameTime& gameTime);
-        void update(const Duration& elapsedTime);
-        void restart();
-        void setInterval(Duration interval);
+        void update(const GameTime& gameTime) {
+            update(gameTime.lastFrame);
+        }
 
-        Timer(T* object, TimerCallback<T> callback);
+        void update(const Duration& elapsedTime) {
+            if (_running) {
+                _runningClock -= elapsedTime;
+                if (_runningClock <= Duration::zero()) {
+                    (_object->*_callback)(_running, _interval);
+                    _runningClock = _interval;
+                    if (!_running) {
+                        return;
+                    }
+                }
+            }
+        }
+
+        void restart();
+
+        void setInterval(Duration interval) {
+            _interval = _runningClock = interval;
+        }
+
+        Timer(T* object, TimerCallback<T> callback)
+            : _object(object), _callback(callback) {}
+
         void operator=(const Timer&)  = delete;
         void operator=(const Timer&&) = delete;
         Timer(Timer&)                 = delete;
         Timer(Timer&&)                = delete;
         ~Timer()                      = default;
     };
-}
+} // namespace SatansLilHelper
--- game/src/Timer.cpp
+++ game/src/Timer.cpp
@@ -4,30 +4,16 @@ namespace SatansLilHelper {
 
     template <typename T>
     void Timer<T>::setInterval(Duration interval) {
-        _interval = _runningClock = interval;
+       
     }
 
     template <typename T>
     void Timer<T>::update(const GameTime& gameTime) {
-        update(gameTime.lastFrame);
-    }
-
-    template <typename T>
-    void Timer<T>::update(const Duration& elapsedTime) {
-        if (_running) {
-            _runningClock -= elapsedTime;
-            if (_runningClock <= Duration::zero()) {
-                _callback(_running, _interval);
-                _runningClock = _interval;
-                if (!_running) {
-                    return;
-                }
-            }
-        }
+ 
     }
 
     template <typename T>
     Timer<T>::Timer(T* object, TimerCallback<T> callback)
-        : _object(object), _callback(callback) {}
+       
 
 }
