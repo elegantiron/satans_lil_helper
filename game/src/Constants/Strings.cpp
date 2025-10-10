62ff1ac9f6144df39524030f23e4bab7ae63c453
elegantiron <elegant.iron3167@fastmail.com>
2025-10-10 19:01:00 -0400

    update xgettext keywords

--- game/src/Constants/Strings.cpp
+++ game/src/Constants/Strings.cpp
@@ -1,17 +1,20 @@
 #include "Constants.hpp"
+#define _(STRING) translate(STRING)
+#define N_(STRING) STRING
+#define C_(CONTEXT, STRING) translate(CONTEXT, STRING)
 
 namespace SatansLilHelper::Constants::Strings {
 
-    extern const basic_message<char> Title = translate("Satan's Lil Helper");
-    extern const basic_message<char> PressStart = translate("Press START");
+    extern const basic_message<char> Title      = _("Satan's Lil Helper");
+    extern const basic_message<char> PressStart = _("Press START");
 
     namespace Menu {
-        extern const basic_message<char> NewGame  = translate("new game");
-        extern const basic_message<char> Bestiary = translate("bestiary");
-        extern const basic_message<char> Settings = translate("settings");
+        extern const basic_message<char> NewGame
+            = C_("section header", "new game");
+        extern const basic_message<char> Bestiary
+            = C_("section header", "bestiary");
+        extern const basic_message<char> Settings
+            = C_("section header", "settings");
     }
 
-    extern const basic_message<char> Inventory = translate("inventory");
-    extern const basic_message<char> InventoryHeader
-        = translate("section", "inventory");
 }
