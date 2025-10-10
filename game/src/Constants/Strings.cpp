ebf1d72a308c95b6270e61b043a4740d170e5f62
elegantiron <elegant.iron3167@fastmail.com>
2025-10-10 18:30:29 -0400

    make strings lower case prepares for using locale to capitalize

--- game/src/Constants/Strings.cpp
+++ game/src/Constants/Strings.cpp
@@ -1,12 +1,17 @@
 #include "Constants.hpp"
 
 namespace SatansLilHelper::Constants::Strings {
+
     extern const basic_message<char> Title = translate("Satan's Lil Helper");
     extern const basic_message<char> PressStart = translate("Press START");
 
     namespace Menu {
-        extern const basic_message<char> NewGame  = translate("New Game");
-        extern const basic_message<char> Bestiary = translate("Bestiary");
-        extern const basic_message<char> Settings = translate("Settings");
+        extern const basic_message<char> NewGame  = translate("new game");
+        extern const basic_message<char> Bestiary = translate("bestiary");
+        extern const basic_message<char> Settings = translate("settings");
     }
+
+    extern const basic_message<char> Inventory = translate("inventory");
+    extern const basic_message<char> InventoryHeader
+        = translate("section", "inventory");
 }
