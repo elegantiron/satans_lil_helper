0316ad77d8b63be980b94f187f053253ae26b79f
elegantiron <128913032+elegantiron@users.noreply.github.com>
2025-10-31 09:10:25 -0400

    add font faces

--- inc/Assets/Fonts.hpp
+++ inc/Assets/Fonts.hpp
@@ -1,7 +1,13 @@
 #pragma once
 #include <SFML/Graphics/Font.hpp>
+#include <libslh/Graphics/Font.hpp>
 
 namespace SatansLilHelper::Assets::Fonts {
     extern const sf::Font FairyDustB;
     extern const sf::Font CrayonLibre;
+
+    namespace Menu {
+        extern const libslh::Font Default;
+        extern const libslh::Font Selected;
+    }
 }
--- src/Assets/Fonts.cpp
+++ src/Assets/Fonts.cpp
@@ -8,11 +8,29 @@ namespace {
     const unsigned char crayon_libre[] = {
 #embed "../../assets/Fonts/CrayonLibre.ttf"
     };
+    constexpr int   MENU_CHARACTER_HEIGHT      = 55;
+    constexpr float DEFAULT_OUTLINE_THICKNESS  = 0.0F;
+    constexpr float SELECTED_OUTLINE_THICKNESS = 0.0F;
 }
 
 namespace SatansLilHelper::Assets::Fonts {
     extern const sf::Font FairyDustB{fairy_dust, sizeof(fairy_dust)};
     extern const sf::Font CrayonLibre{crayon_libre, sizeof(crayon_libre)};
+
+    namespace Menu {
+        extern const libslh::Font Default{CrayonLibre,
+                                          MENU_CHARACTER_HEIGHT,
+                                          sf::Color::White,
+                                          DEFAULT_OUTLINE_THICKNESS,
+                                          sf::Color::Black,
+                                          sf::Text::Regular};
+        extern const libslh::Font Selected{CrayonLibre,
+                                           MENU_CHARACTER_HEIGHT,
+                                           sf::Color::Red,
+                                           SELECTED_OUTLINE_THICKNESS,
+                                           sf::Color::Black,
+                                           sf::Text::Regular};
+    }
 }
 
 // NOLINTEND(*c-arrays,*pointer-decay)
